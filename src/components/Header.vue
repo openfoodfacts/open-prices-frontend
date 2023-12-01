7516e179f7647b100a5bb8f637d6840597392956
Raphael Odini <raphodn@users.noreply.github.com>
2023-12-01 15:57:29 +0100

    Cleanup header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -37,82 +37,7 @@
         </div>
         <div class="hidden md:block">
           <div class="flex items-center ml-4 md:ml-6">
-            <button
-              class="p-1 text-gray-400 border-2 border-transparent rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-gray-700"
-              aria-label="Notifications"
-            >
-              <svg
-                class="w-6 h-6"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                />
-              </svg>
-            </button>
-
-            <!-- Profile dropdown -->
             <div class="relative ml-3">
-              <div>
-                <button
-                  class="flex items-center max-w-xs text-sm text-white rounded-full focus:outline-none focus:shadow-solid"
-                  id="user-menu"
-                  aria-label="User menu"
-                  aria-haspopup="true"
-                  @click="showProfileMenu = !showProfileMenu"
-                >
-                  <img
-                    class="w-8 h-8 rounded-full"
-                    src="/img/person1.jpeg"
-                    alt=""
-                  />
-                </button>
-              </div>
-
-              <transition
-                enter-active-class="transition duration-100 ease-out"
-                enter-from-class="transform scale-95 opacity-0"
-                enter-to-class="transform scale-100 opacity-100"
-                leave-active-class="transition duration-75 ease-in"
-                leave-from-class="transform scale-100 opacity-100"
-                leave-to-class="transform scale-95 opacity-0"
-              >
-                <div
-                  v-if="showProfileMenu"
-                  class="absolute right-0 w-48 mt-2 origin-top-right rounded-md shadow-lg"
-                >
-                  <div
-                    class="py-1 bg-white rounded-md ring-1 ring-black ring-opacity-5"
-                    role="menu"
-                    aria-orientation="vertical"
-                    aria-labelledby="user-menu"
-                  >
-                    <a
-                      href="#"
-                      class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                      role="menuitem"
-                      >Your Profile</a
-                    >
-                    <a
-                      href="#"
-                      class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                      role="menuitem"
-                      >Settings</a
-                    >
-                    <a
-                      href="#"
-                      class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                      role="menuitem"
-                      >Sign out</a
-                    >
-                  </div>
-                </div>
-              </transition>
             </div>
           </div>
         </div>
@@ -184,55 +109,19 @@
           >
         </router-link>
       </div>
-      <div class="pt-4 pb-3 border-t border-gray-700">
-        <div class="flex items-center px-5">
-          <div class="flex-shrink-0">
-            <img
-              class="w-10 h-10 rounded-full"
-              src="/img/person1.jpeg"
-              alt=""
-            />
-          </div>
-          <div class="ml-3">
-            <div class="text-base font-medium leading-none text-white">
-              Tom Cook
-            </div>
-            <div class="mt-1 text-sm font-medium leading-none text-gray-400">
-              tom@example.com
-            </div>
-          </div>
-        </div>
-        <div class="px-2 mt-3">
-          <a
-            href="#"
-            class="block px-3 py-2 text-base font-medium text-gray-400 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-            >Your Profile</a
-          >
-          <a
-            href="#"
-            class="block px-3 py-2 mt-1 text-base font-medium text-gray-400 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-            >Settings</a
-          >
-          <a
-            href="#"
-            class="block px-3 py-2 mt-1 text-base font-medium text-gray-400 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-            >Sign out</a
-          >
-        </div>
-      </div>
     </div>
   </nav>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   data: () => ({
     showMenu: false,
     showProfileMenu: false,
     links: [
-    { text: "Home", to: "/" },
+    { text: 'Home', to: '/' },
     { text: 'Add a price', to: '/add' },
     ],
   }),
