dd369732d22558ea122499a0cd51fe41924bb458
Raphael Odini <raphodn@users.noreply.github.com>
2023-12-10 11:40:01 +0100

    feat: Basic prices create form (#13)

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import Home from './views/Home.vue'
 import SignIn from './views/SignIn.vue'
-import AddPrice from './views/AddPrice.vue'
+import AddPriceHome from './views/AddPriceHome.vue'
+import AddPriceSingle from './views/AddPriceSingle.vue'
 import NotFound from './views/NotFound.vue'
 
 /** @type {import('vue-router').RouterOptions['routes']} */
 export let routes = [
   { path: '/', name: 'home', component: Home, meta: { title: 'Open Prices' } },
   { path: '/sign-in', name: 'sign-in', component: SignIn, meta: { title: 'Sign in' } },
-  { path: '/add', name: 'add-price', component: AddPrice, meta: { title: 'Add a price', requiresAuth: true } },
+  { path: '/add', name: 'add-price', component: AddPriceHome, meta: { title: 'Add a price', requiresAuth: true }},
+  { path: '/add/single', name: 'add-price-single', component: AddPriceSingle, meta: { title: 'Add a single price', requiresAuth: true }},
   { path: '/:path(.*)', component: NotFound },
 ]
---
+++ src/views/AddPriceHome.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-row>
+    <v-col cols="12" md="6">
+      <v-card
+        title="Single product mode"
+        subtitle="Get your camera ready :)"
+        prepend-icon="mdi-barcode"
+        to="/add/single"></v-card>
+    </v-col>
+    <v-col cols="12" md="6">
+      <v-card
+        title="Receipt mode"
+        subtitle="Coming in the v2 (help us!)"
+        prepend-icon="mdi-receipt-text-outline"
+        link
+        disabled></v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {}
+</script>
