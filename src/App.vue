d3b03f085fb30c431605ab266ce6e08d6653a805
Raphael Odini <raphodn@users.noreply.github.com>
2023-12-14 17:33:00 +0100

    Basic footer component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
   <v-app>
-    <v-layout class="rounded rounded-md">
-      <Header />
+    <Header />
 
-      <v-main class="d-flex justify-center">
-        <v-container>
-          <router-view />
-        </v-container>
-      </v-main>
-    </v-layout>
+    <v-main class="d-flex justify-center">
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-main>
+
+    <Footer />
   </v-app>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
 import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 export default defineComponent({
   components: {
     Header,
+    Footer,
   },
 })
 </script>
