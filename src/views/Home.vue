40bcecf50973080b8b56071adcfe19296bb29510
Raphael Odini <raphodn@users.noreply.github.com>
2023-12-15 15:10:00 +0100

    feat: add a footer (#20)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,6 @@
 <template>
-  <p>Welcome to Open Prices.</p>
-  <router-link to="/add" class="text-indigo-600 underline hover:text-indigo-500">Add a price</router-link>.
+  <h3>Welcome to Open Prices!</h3>
+  <p>an open crowdsourced database of food prices 🏷🍊💲</p>
+  <br />
+  <router-link to="/add" class="text-indigo-600 underline hover:text-indigo-500">Add a price</router-link>
 </template>
