1ffbe68ed3b96f6e32c265690d1322d69b809c4b
Raphael Odini <raphodn@users.noreply.github.com>
2023-12-19 12:01:37 +0100

    Create PriceCard

---
+++ src/components/PriceCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card>
+    <v-card-item>
+      <template v-slot:prepend>
+        <v-avatar v-if="price.product" :image="price.product.image_url || defaultAvatar"></v-avatar>
+        <v-avatar v-if="!price.product" :image="defaultAvatar"></v-avatar>
+      </template>
+
+      <v-card-title v-if="price.product">{{ price.product.product_name }}</v-card-title>
+      <v-card-title v-if="!price.product">{{ price.product_code }}</v-card-title>
+
+      <v-card-subtitle v-if="price.product">{{ price.product.product_quantity }} · {{ price.product.brands }}</v-card-subtitle>
+      <v-card-subtitle v-if="!price.product"></v-card-subtitle>
+    </v-card-item>
+
+    <v-card-text>
+      <span>{{ price.price }} {{ price.currency }}</span>
+      <span> · </span>
+      <span v-if="price.location">{{ price.location.osm_name }}, {{ price.location.osm_address_city }}</span>
+      <span v-if="!price.location">{{ price.location_id }}</span>
+      <span> · </span>
+      <span>{{ price.date }}</span>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ['price'],
+  data() {
+    return {
+      defaultAvatar: 'https://world.openfoodfacts.org/images/icons/dist/packaging.svg'
+    }
+  }
+}
+</script>
--- src/views/PriceList.vue
+++ src/views/PriceList.vue
@@ -2,38 +2,23 @@
   <h1 class="mb-1">Last prices</h1>
 
   <v-row>
-    <v-col cols="12" sm="6" md="4" v-for="p in prices" :key="p">
-      <v-card elevation="1">
-        <v-card-item>
-          <template v-slot:prepend>
-            <v-avatar v-if="p.product" :image="p.product.image_url || defaultAvatar"></v-avatar>
-            <v-avatar v-if="!p.product" :image="defaultAvatar"></v-avatar>
-          </template>
-          <v-card-title v-if="p.product">{{ p.product.product_name }}</v-card-title>
-          <v-card-title v-if="!p.product">{{ p.product_code }}</v-card-title>
-          <v-card-subtitle v-if="p.product">{{ p.product.product_quantity }} · {{ p.product.brands }}</v-card-subtitle>
-        </v-card-item>
-        <v-card-text>
-          <span>{{ p.price }} {{ p.currency }}</span>
-          <span> · </span>
-          <span v-if="p.location">{{ p.location.osm_name }}, {{ p.location.osm_address_city }}</span>
-          <span v-if="!p.location">{{ p.location_id }}</span>
-          <span> · </span>
-          <span>{{ p.date }}</span>
-        </v-card-text>
-      </v-card>
+    <v-col cols="12" sm="6" md="4" v-for="price in prices" :key="price">
+      <PriceCard :price="price" elevation="1" height="100%"></PriceCard>
     </v-col>
   </v-row>
 </template>
 
 <script>
 import api from '../services/api'
+import PriceCard from '../components/PriceCard.vue'
 
 export default {
+  components: {
+    PriceCard,
+  },
   data() {
     return {
       prices: [],
-      defaultAvatar: 'https://world.openfoodfacts.org/images/icons/dist/packaging.svg'
     }
   },
   mounted() {
