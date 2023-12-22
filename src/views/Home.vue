74686a1f7bdccca0d9fb3979648227fc88f7dd9a
Raphael Odini <raphodn@users.noreply.github.com>
2023-12-23 00:54:51 +0100

    fix(home): replace link with button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <h1 class="mb-1">Welcome to Open Prices!</h1>
-  <p>an open crowdsourced database of food prices 🏷🍊💲</p>
+  <p>An open crowdsourced database of food prices 🏷🍊💲</p>
   <br />
-  <router-link to="/add" class="text-indigo-600 underline hover:text-indigo-500">Add a price</router-link>
+  <v-btn prepend-icon="mdi-plus" to="/add">Add a price</v-btn>
 </template>
