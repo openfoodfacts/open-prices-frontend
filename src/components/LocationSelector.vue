1f5a9ff6671141fae1c9bb3f1c4d412d157a6f83
Raphael Odini <raphodn@users.noreply.github.com>
2023-12-23 23:00:38 +0100

    feat(price create form): add leaflet map to location selector (#57)
    * Add leaflet & vue-leaflet
    
    * Map with icons. fitBounds
    
    * Better display location results (name, city)

--- src/components/LocationSelector.vue
+++ src/components/LocationSelector.vue
@@ -27,15 +27,31 @@
 
       <v-card-text v-if="results && Array.isArray(results)">
         <h3>Results <small>{{ results.length }}</small></h3>
-        <div class="d-flex flex-wrap ga-2">
-          <v-card
-            v-for="location in results"
-            elevation="1"
-            @click="selectLocation(location)"
-          >
-            <v-card-text>{{ location.display_name }}</v-card-text>
-          </v-card>
-        </div>
+        <v-row>
+          <v-col cols="12" sm="6">
+            <div class="d-flex flex-wrap ga-2">
+              <v-card
+                v-for="location in results"
+                elevation="1"
+                @click="selectLocation(location)"
+              >
+                <v-card-title>{{ getNominatimLocationTitle(location) }}</v-card-title>
+                <v-card-text>{{ location.display_name }} <strong>[{{ location.type }}]</strong></v-card-text>
+              </v-card>
+            </div>
+          </v-col>
+          <v-col cols="12" sm="6" style="min-height:200px">
+            <l-map ref="map" v-model:zoom="mapZoom" :center="mapCenter" :use-global-leaflet="false" @ready="initMap">
+              <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base" name="OpenStreetMap"></l-tile-layer>
+              <l-marker v-for="location in results" :lat-lng="[location.lat, location.lon]">
+                <l-popup>
+                  {{ getNominatimLocationTitle(location, true) }}<br />
+                  <strong>[{{ location.type }}]</strong>
+                </l-popup>
+              </l-marker>
+            </l-map>
+          </v-col>
+        </v-row>
       </v-card-text>
       <v-card-text v-if="results && (typeof results === 'string')">{{ results }}</v-card-text>
 
@@ -69,17 +85,31 @@
 </template>
 
 <script>
+import "leaflet/dist/leaflet.css"
+import { LMap, LTileLayer, LMarker, LPopup } from "@vue-leaflet/vue-leaflet"
 import api from '../services/api'
 
 export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LPopup,
+  },
   data() {
     return {
+      // location form
       locationSearchForm: {
         q: ''
       },
       loading: false,
       results: null,
-      recentLocations: api.getRecentLocations()
+      recentLocations: api.getRecentLocations(),
+      // map
+      map: null,
+      mapZoom: 5,
+      mapCenter: [45, 5],
+      mapBounds: null
     }
   },
   computed: {
@@ -94,6 +124,12 @@ export default {
     fieldRequired(v) {
       return !!v
     },
+    initMap() {  // TODO: improve map setup
+      this.map = this.$refs.map.leafletObject
+      if (this.mapBounds) {
+        this.map.fitBounds(this.mapBounds)
+      }
+    },
     search() {
       this.results = null
       this.loading = true
@@ -102,10 +138,32 @@ export default {
         this.loading = false
         if (data.length) {
           this.results = data
+          if (this.results.length > 1) {
+            this.mapBounds = this.results.map(l => [l.lat, l.lon])
+          } else {
+            this.mapCenter = this.results.map(l => [l.lat, l.lon])[0]
+            this.mapZoom = 12
+            this.mapBounds = null
+          }
         } else {
           this.results = 'No results found'
         }
       })
+    },
+    getNominatimLocationCity(location) {
+      if (location.address) {
+        return location.address.village || location.address.town || location.address.city || location.address.municipality
+      }
+    },
+    getNominatimLocationTitle(location, withRoad=false) {
+      let locationTitle = location.name
+      if (location.address) {
+        if (withRoad) {
+          locationTitle += `, ${location.address.road}`
+        }
+        locationTitle += `, ${this.getNominatimLocationCity(location)}`
+      }
+      return locationTitle
     },
     clearRecentLocations() {
       api.clearRecentLocations()
