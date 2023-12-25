7ec15a2058170840d73d8d3132b37640c20cec81
Raphael Odini <raphodn@users.noreply.github.com>
2023-12-25 11:19:15 +0100

    Fetch user last prices & price count

--- src/views/UserDashboard.vue
+++ src/views/UserDashboard.vue
@@ -3,15 +3,60 @@
     Dashboard
     <v-progress-circular v-if="loading" indeterminate :size="30"></v-progress-circular>
   </h1>
+
+  <v-row>
+    <v-col cols="12" sm="6">
+      <v-card :title="username" prepend-icon="mdi-account"></v-card>
+    </v-col>
+  </v-row>
+
+  <br />
+
+  <h2 class="mb-1">
+    Last prices
+    <small>{{ userPriceCount }}</small>
+  </h2>
+
+  <v-row>
+    <v-col cols="12" sm="6" md="4" v-for="price in userPriceList" :key="price">
+      <PriceCard :price="price" :product="price.product" elevation="1" height="100%"></PriceCard>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
+import api from '../services/api'
+import PriceCard from '../components/PriceCard.vue'
+
 export default {
+  components: {
+    PriceCard,
+  },
   data() {
     return {
-      prices: [],
+      userPriceList: [],
+      userPriceCount: null,
       loading: false,
     }
   },
+  computed: {
+    username() {
+      return api.getUsername()
+    },
+  },
+  mounted() {
+    this.getUserPrices()
+  },
+  methods: {
+    getUserPrices() {
+      this.loading = true
+      return api.getPrices({ owner: this.username, order_by: '-created' })
+        .then((data) => {
+          this.userPriceList = data.items
+          this.userPriceCount = data.total
+          this.loading = false
+        })
+    },
+  }
 }
 </script>
