aa81347c601b23366db64b83953877a860e5c3c2
Raphael Odini <raphodn@users.noreply.github.com>
2023-12-25 11:13:36 +0100

    New route /dashboard, new view UserDashboard

---
+++ src/views/UserDashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1 class="mb-1">
+    Dashboard
+    <v-progress-circular v-if="loading" indeterminate :size="30"></v-progress-circular>
+  </h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      prices: [],
+      loading: false,
+    }
+  },
+}
+</script>
