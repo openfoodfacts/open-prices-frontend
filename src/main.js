50f18c8c4096f3d63a9b19c92626b7c0c12d6f41
Raphael Odini <raphodn@users.noreply.github.com>
2023-12-26 01:28:46 +0100

    Add pinia plugin to persist store to localStorage

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import './assets/main.css'
 import App from './App.vue'
 import router from './router.js'
 import { vuetify } from './plugins/vuetify.js'
 
 const app = createApp(App)
+
 const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
 app.use(pinia)
 app.use(router)
