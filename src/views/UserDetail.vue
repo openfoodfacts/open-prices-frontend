0689f911322c089031aaa32e3b3629b72b1d5043
Raphael Odini <raphodn@users.noreply.github.com>
2023-12-28 18:40:31 +0100

    new UserDetail page

---
+++ src/views/UserDetail.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-row>
+    <v-col cols="12" sm="6">
+      <v-card :title="username" prepend-icon="mdi-account"></v-card>
+    </v-col>
+  </v-row>
+
+  <br />
+
+  <h2 class="mb-1">
+    Latest prices
+    <small>{{ userPriceCount }}</small>
+  </h2>
+
+  <v-row>
+    <v-col cols="12" sm="6" md="4" v-for="price in userPriceList" :key="price">
+      <PriceCard :price="price" :product="price.product" elevation="1" height="100%"></PriceCard>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { mapStores } from 'pinia'
+import { useAppStore } from '../store'
+import api from '../services/api'
+import PriceCard from '../components/PriceCard.vue'
+
+export default {
+  components: {
+    PriceCard,
+  },
+  data() {
+    return {
+      userPriceList: [],
+      userPriceCount: null,
+      loading: false,
+    }
+  },
+  computed: {
+    ...mapStores(useAppStore),
+    username() {
+      return this.$route.params.username
+    },
+  },
+  mounted() {
+    this.getUserPrices()
+  },
+  methods: {
+    getUserPrices() {
+      this.loading = true
+      return api.getPrices({ owner: this.username, order_by: '-created' })
+        .then((data) => {
+          this.userPriceList = data.items
+          this.userPriceCount = data.total
+          this.loading = false
+        })
+    },
+  }
+}
+</script>
