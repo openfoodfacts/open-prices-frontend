3882423bdb90d16bb5e892335459c94c7b5f9e26
Raphael Odini <raphodn@users.noreply.github.com>
2024-01-07 21:59:14 +0100

    New brands detail page

---
+++ src/views/BrandDetail.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-row>
+    <v-col cols="12" sm="6">
+      <v-card
+        :title="brand"
+        prepend-icon="mdi-database-outline">
+      </v-card>
+    </v-col>
+  </v-row>
+
+  <v-row class="mt-0">
+    <v-col cols="12" sm="6">
+      <v-btn size="small" append-icon="mdi-open-in-new" :href="getBrandOFFUrl()" target="_blank">
+        Open Food Facts
+      </v-btn>
+    </v-col>
+  </v-row>
+
+  <br />
+
+  <h2 class="mb-1">
+    Top products
+    <small>{{ brandProductTotal }}</small>
+    <v-progress-circular v-if="loading" indeterminate :size="30"></v-progress-circular>
+  </h2>
+
+  <v-row>
+    <v-col cols="12" sm="6" md="4" v-for="product in brandProductList" :key="product">
+      <PriceCard :product="product" elevation="1" height="100%"></PriceCard>
+    </v-col>
+  </v-row>
+
+  <v-row v-if="brandProductList.length < brandProductTotal" class="mb-2">
+    <v-col align="center">
+      <v-btn size="small" @click="getBrandProducts">Load more</v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import api from '../services/api'
+import PriceCard from '../components/PriceCard.vue'
+
+export default {
+  components: {
+    PriceCard,
+  },
+  data() {
+    return {
+      brand: this.$route.params.id,
+      brandProductList: [],
+      brandProductTotal: null,
+      brandProductPage: 0,
+      loading: false,
+    }
+  },
+  mounted() {
+    this.getBrandProducts()
+  },
+  methods: {
+    getBrandProducts() {
+      this.loading = true
+      this.brandProductPage += 1
+      return api.getProducts({ brands__like: this.brand, unique_scans_n__gte: 1, order_by: '-unique_scans_n', page: this.brandProductPage })
+        .then((data) => {
+          this.brandProductList.push(...data.items)
+          this.brandProductTotal = data.total
+          this.loading = false
+        })
+    },
+    getBrandOFFUrl() {
+      return `https://world.openfoodfacts.org/brand/${this.brand}`
+    }
+  }
+}
+</script>
