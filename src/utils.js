a63b3d986980e110e6a8f48f6e3d402993c2372b
Raphael Odini <raphodn@users.noreply.github.com>
2024-01-07 09:06:12 +0100

    Improve oldest date relative display

--- src/utils.js
+++ src/utils.js
@@ -30,21 +30,21 @@ function prettyDate(dateString) {
  * https://johnresig.com/blog/javascript-pretty-date/
  * input: '2023-12-27T17:08:19.021410+01:00'
  * output: '5 hours ago', 'Yesterday', '2 days ago'...
+ * changes: add short (replace 'days' with 'd', remove 'Yesterday'), extend days & weeks
  */
 function prettyRelativeDateTime(dateTimeString, short=false) {
   var date = new Date((dateTimeString || "").replace(/-/g, "/").replace(/[TZ]/g, " ")),
       diff = (((new Date()).getTime() - date.getTime()) / 1000),
       day_diff = Math.floor(diff / 86400);
 
-  if (isNaN(day_diff) || day_diff < 0 || day_diff >= 31) return;
+  if (isNaN(day_diff) || day_diff < 0) return;
 
   if (short) {
-    // replace 'days' with 'd', remove 'Yesterday'
     return day_diff == 0 && (
-      diff < 60 && "just now" || diff < 120 && "1m ago" || diff < 3600 && Math.floor(diff / 60) + "m ago" || diff < 7200 && "1h ago" || diff < 86400 && Math.floor(diff / 3600) + "h ago") || day_diff < 7 && day_diff + "d ago" || day_diff < 31 && Math.ceil(day_diff / 7) + "w ago";
+      diff < 60 && "just now" || diff < 120 && "1m ago" || diff < 3600 && Math.floor(diff / 60) + "m ago" || diff < 7200 && "1h ago" || diff < 86400 && Math.floor(diff / 3600) + "h ago") || day_diff < 10 && day_diff + "d ago" || Math.ceil(day_diff / 7) + "w ago";
   }
   return day_diff == 0 && (
-  diff < 60 && "just now" || diff < 120 && "1 minute ago" || diff < 3600 && Math.floor(diff / 60) + " minutes ago" || diff < 7200 && "1 hour ago" || diff < 86400 && Math.floor(diff / 3600) + " hours ago") || day_diff == 1 && "Yesterday" || day_diff < 7 && day_diff + " days ago" || day_diff < 31 && Math.ceil(day_diff / 7) + " weeks ago";
+  diff < 60 && "just now" || diff < 120 && "1 minute ago" || diff < 3600 && Math.floor(diff / 60) + " minutes ago" || diff < 7200 && "1 hour ago" || diff < 86400 && Math.floor(diff / 3600) + " hours ago") || day_diff == 1 && "Yesterday" || day_diff < 10 && day_diff + " days ago" || Math.ceil(day_diff / 7) + " weeks ago";
 }
 
 function getCategory(categoryId) {
