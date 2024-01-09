7abadd6c5cb0b33d5209e52516ad92ebdd90c368
Raphael Odini <raphodn@users.noreply.github.com>
2024-01-10 00:20:25 +0100

    feat(user settings): new user settings page (#121)
    * New user settings page
    
    * New store action setLastCurrencyUsed

--- src/services/api.js
+++ src/services/api.js
@@ -34,7 +34,7 @@ export default {
   createPrice(priceData) {
     const store = useAppStore()
     store.user.last_product_mode_used = priceData.product_code ? 'barcode' : 'category'
-    store.user.last_currency_used = priceData.currency
+    store.setLastCurrencyUsed(priceData.currency)
     return fetch(`${import.meta.env.VITE_OPEN_PRICES_API_URL}/prices`, {
       method: 'POST',
       headers: {
