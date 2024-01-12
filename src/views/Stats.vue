b721b802de813a6e6dec8f773b1694874985ff78
Raphael Odini <raphodn@users.noreply.github.com>
2024-01-12 15:07:49 +0100

    fix(stats): typo again in product total. ref #134

--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -10,7 +10,7 @@
         <v-card-text>
           <p>
             Total
-            <strong>{{ productTotal }}</strong>
+            <strong>{{ priceTotal }}</strong>
           </p>
           <p>
             With a product <v-chip label size="small" density="comfortable" class="mr-1">barcode</v-chip>
@@ -28,7 +28,7 @@
         <v-card-text>
           <p>
             Total
-            <strong>{{ priceTotal }}</strong>
+            <strong>{{ productTotal }}</strong>
           </p>
           <p>
             With a price
