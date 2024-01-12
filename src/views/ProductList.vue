ba861aad01bda3d4055243fe3645bb577e3a5841
Raphael Odini <raphodn@users.noreply.github.com>
2024-01-12 11:23:15 +0100

    feat(product list): add filter to hide products with prices (#130)
    * Product list: simple filter button to hide products with prices
    
    * Move buttons to row below. Improve active style

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -2,18 +2,33 @@
   <h1 class="mb-1">
     Top products
     <v-progress-circular v-if="loading" indeterminate :size="30"></v-progress-circular>
-
-    <v-menu v-if="!loading">
-      <template v-slot:activator="{ props }">
-        <v-btn v-bind="props" size="small" prepend-icon="mdi-arrow-down" :append-icon="getCurrentProductOrderIcon">Order</v-btn>
-      </template>
-      <v-list>
-        <v-list-item :slim="true" v-for="order in productOrderList" :key="order.key" :prepend-icon="order.icon" :active="productOrder === order.key" @click="selectProductOrder(order.key)">
-          {{ order.value }}
-        </v-list-item>
-      </v-list>
-    </v-menu>
   </h1>
+
+  <v-row v-if="!loading">
+    <v-col>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn v-bind="props" size="small" class="mr-2" prepend-icon="mdi-filter-variant" :active="!!productFilter">Filter</v-btn>
+        </template>
+        <v-list>
+          <v-list-item :slim="true" v-for="filter in productFilterList" :key="filter.key" :prepend-icon="(productFilter === filter.key) ? 'mdi-check-circle' : 'mdi-circle-outline'" :active="productFilter === filter.key" @click="toggleProductFilter(filter.key)">
+            {{ filter.value }}
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn v-bind="props" size="small" prepend-icon="mdi-arrow-down" :append-icon="getCurrentProductOrderIcon"  :active="!!productOrder">Order</v-btn>
+        </template>
+        <v-list>
+          <v-list-item :slim="true" v-for="order in productOrderList" :key="order.key" :prepend-icon="order.icon" :active="productOrder === order.key" @click="selectProductOrder(order.key)">
+            {{ order.value }}
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-col>
+  </v-row>
 
   <v-row>
     <v-col cols="12" sm="6" md="4" v-for="product in productList" :key="product">
@@ -38,6 +53,10 @@ export default {
   },
   data() {
     return {
+      productFilter: '',
+      productFilterList: [
+        {key: 'hide_price_count_gte_1', value: 'Hide products with prices'},
+      ],
       productOrder: '-unique_scans_n',
       productOrderList: [
         {key: '-unique_scans_n', value: 'Number of scans', icon: 'mdi-barcode-scan'},
@@ -56,7 +75,14 @@ export default {
     getCurrentProductOrderIcon() {
       let currentProductOrder = this.productOrderList.find(o => o.key === this.productOrder)
       return currentProductOrder ? currentProductOrder.icon : ''
-    }
+    },
+    getProductsParams() {
+      let defaultParams = { page: this.productPage, order_by: `${this.productOrder}` }
+      if (this.productFilter && this.productFilter === 'hide_price_count_gte_1') {
+        defaultParams['price_count'] = 0
+      }
+      return defaultParams
+    },
   },
   methods: {
     initProductList() {
@@ -67,12 +93,16 @@ export default {
     getProducts() {
       this.loading = true
       this.productPage += 1
-      return api.getProducts({ page: this.productPage, order_by: `${this.productOrder}` })
+      return api.getProducts(this.getProductsParams)
         .then((data) => {
           this.productList.push(...data.items)
           this.productTotal = data.total
           this.loading = false
         })
+    },
+    toggleProductFilter(filterKey) {
+      this.productFilter = this.productFilter ? '' : filterKey
+      this.initProductList()
     },
     selectProductOrder(orderKey) {
       if (this.productOrder !== orderKey) {
