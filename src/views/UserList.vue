825e4443835675586ce5b5116c6b5dc2b5b7dcd2
Raphael Odini <raphodn@users.noreply.github.com>
2024-01-14 20:29:28 +0100

    feat(user list): new user list page ordered by price_count (#150)
    * New page UserList ordered by price count
    
    * Add to routing & drawer
    
    * Only show users with at least 1 price

---
+++ src/views/UserList.vue
@@ -0,0 +1,85 @@
+<template>
+  <h1 class="mb-1">
+    Top contributors
+    <v-progress-circular v-if="loading" indeterminate :size="30"></v-progress-circular>
+  </h1>
+
+  <v-row>
+    <v-col cols="12" sm="6" md="4" v-for="user in userList" :key="user">
+      <v-card
+        :title="user.user_id"
+        prepend-icon="mdi-account"
+        elevation="1"
+        height="100%"
+        @click="goToUser(user)">
+        <v-card-text>
+          <v-chip label size="small" density="comfortable" :color="getUserPriceCountColor(user)" class="mr-1">
+            <v-icon start icon="mdi-tag-outline"></v-icon>
+            {{ user.price_count }} prices
+          </v-chip>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+
+  <v-row v-if="userList.length < userTotal" class="mb-2">
+    <v-col align="center">
+      <v-btn size="small" @click="getUsers">Load more</v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import api from '../services/api'
+import utils from '../utils.js'
+
+export default {
+  data() {
+    return {
+      // data
+      userList: [],
+      userTotal: null,
+      userPage: 0,
+      loading: false,
+    }
+  },
+  computed: {},
+  mounted() {
+    this.initUserList()
+  },
+  methods: {
+    initUserList() {
+      this.userList = []
+      this.userPage = 0
+      this.getUsers()
+    },
+    getUsers() {
+      this.loading = true
+      this.userPage += 1
+      return api.getUsers({ price_count__gte: 1, order_by: '-price_count', page: this.userPage })
+        .then((data) => {
+          this.userList.push(...data.items)
+          this.userTotal = data.total
+          this.loading = false
+        })
+    },
+    getUserTitle(user) {
+      return utils.getUserTitle(user, true, false, true, true)
+    },
+    getUserPriceCountColor(user) {
+      if (user.price_count === 0) {
+        return 'error'
+      }
+      if (user.price_count === 1) {
+        return 'warning'
+      }
+      if (user.price_count > 1) {
+        return 'success'
+      }
+    },
+    goToUser(user) {
+      this.$router.push({ path: `/users/${user.id}` })
+    },
+  }
+}
+</script>
