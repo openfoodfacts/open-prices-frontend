775c067064a7d48921c498a02ecac1dda5f0fb8f
Raphael Odini <raphodn@users.noreply.github.com>
2024-01-15 08:32:58 +0100

    Add new stats on user total

--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -46,6 +46,25 @@
             Total
             <strong>{{ locationTotal }}</strong>
           </p>
+          <p>
+            With a price
+            <strong>{{ locationWithPriceTotal }}</strong>
+          </p>
+        </v-card-text>
+      </v-card>
+    </v-col>
+
+    <v-col cols="12" md="6" lg="4">
+      <v-card title="Users" height="100%">
+        <v-card-text>
+          <p>
+            Total
+            <strong>{{ userTotal }}</strong>
+          </p>
+          <p>
+            With a price
+            <strong>{{ userWithPriceTotal }}</strong>
+          </p>
         </v-card-text>
       </v-card>
     </v-col>
@@ -64,6 +83,9 @@ export default {
       productTotal: null,
       productWithPriceTotal: null,
       locationTotal: null,
+      locationWithPriceTotal: null,  // same
+      userTotal: null,
+      userWithPriceTotal: null,
       loading: false,
     }
   },
@@ -78,6 +100,9 @@ export default {
     this.getProducts()
     this.getProductsWithPrice()
     this.getLocations()
+    this.getLocationsWithPrice()
+    this.getUsers()
+    this.getUsersWithPrice()
   },
   methods: {
     getPrices() {
@@ -117,6 +142,30 @@ export default {
       return api.getLocations({ size: 1 })
         .then((data) => {
           this.locationTotal = data.total
+          this.loading = false
+        })
+    },
+    getLocationsWithPrice() {
+      this.loading = true
+      return api.getLocations({ price_count__gte: 1,size: 1 })
+        .then((data) => {
+          this.locationWithPriceTotal = data.total
+          this.loading = false
+        })
+    },
+    getUsers() {
+      this.loading = true
+      return api.getUsers({ size: 1 })
+        .then((data) => {
+          this.userTotal = data.total
+          this.loading = false
+        })
+    },
+    getUsersWithPrice() {
+      this.loading = true
+      return api.getUsers({ price_count__gte: 1, size: 1 })
+        .then((data) => {
+          this.userWithPriceTotal = data.total
           this.loading = false
         })
     }
