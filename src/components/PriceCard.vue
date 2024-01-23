openfoodfacts/open-prices-frontend

commit f8f6b70c7bb5989a3abeae97e9b12a9cd7feef07

feat(search): basic product search page by barcode (#195)
* New Search view with product barcode search

* Result: show product's latest price

* Create PricePrice & PriceFooter components

* Add message if product not found

src/components/PriceCard.vue
@@ -33,40 +33,11 @@
             </span>
           </p>
 
-          <p v-if="price">
-            <span class="mr-1">{{ getPriceValueDisplay(priceValue) }}</span>
-            <span v-if="hasProductQuantity" class="mr-1">({{ getPricePerKilo() }})</span>
-            <span v-if="priceWithoutDiscountValue">
-              <v-chip class="mr-1" color="red" variant="outlined" size="small" density="comfortable">
-                {{ $t('PriceCard.Discount') }}
-                <v-tooltip activator="parent" location="top">{{ $t('PriceCard.FullPrice') }} {{ getPriceValueDisplay(priceWithoutDiscountValue) }}</v-tooltip>
-              </v-chip>
-            </span>
-            <i18n-t v-if="!hidePriceDate" keypath="PriceCard.PriceDate" tag="span">
-              <template v-slot:date>
-                <i>{{ getDateFormatted(price.date) }}</i>
-              </template>
-            </i18n-t>
-          </p>
+          <PricePrice v-if="price" :price="price" :productQuantity="product.product_quantity" :hidePriceDate="hidePriceDate"></PricePrice>
         </v-col>
       </v-row>
 
-      <div v-if="price && !hidePriceFooter" class="d-flex flex-wrap ga-1 mt-2">
-        <v-chip v-if="!hidePriceLocation" class="mr-1" label size="small" density="comfortable" @click="goToLocation()">
-          <v-icon start icon="mdi-map-marker-outline"></v-icon>
-          {{ getPriceLocationTitle() }}
-          <span v-if="priceLocationEmoji" style="margin-inline-start:5px">{{ priceLocationEmoji }}</span>
-        </v-chip>
-        <v-chip class="mr-1" label size="small" density="comfortable" @click="goToUser()">
-          <v-icon start icon="mdi-account"></v-icon>
-          {{ price.owner }}
-        </v-chip>
-        <v-chip label size="small" density="comfortable">
-          <v-icon start icon="mdi-clock-outline"></v-icon>
-          {{ getRelativeDateTimeFormatted(price.created) }}
-          <v-tooltip activator="parent" location="top">{{ getDateTimeFormatted(price.created) }}</v-tooltip>
-        </v-chip>
-      </div>
+      <PriceFooter v-if="price && !hidePriceFooter" class="mt-2" :price="price" :hidePriceLocation="hidePriceLocation" :readonly="readonly"></PriceFooter>
     </v-container>
   </v-card>
 </template>
@@ -75,9 +46,14 @@
 import utils from '../utils.js'
 import OriginTags from '../data/origins-tags.json'
 import LabelsTags from '../data/labels-tags.json'
-
+import PricePrice from '../components/PricePrice.vue'
+import PriceFooter from '../components/PriceFooter.vue'
 
 export default {
+  components: {
+    PricePrice,
+    PriceFooter
+  },
   props: {
     'price': null,
     'product': null,
@@ -94,22 +70,12 @@ export default {
       productImageDefault: 'https://world.openfoodfacts.org/images/icons/dist/packaging.svg',
       priceOrigin: null,
       priceLabels: [],
-      priceLocationEmoji: null
     }
   },
   mounted() {
     this.initPriceCard()
   },
   computed: {
-    priceValue() {
-      return parseFloat(this.price.price)
-    },
-    priceWithoutDiscountValue() {
-      return parseFloat(this.price.price_without_discount)
-    },
-    priceCurrency() {
-      return this.price.currency
-    },
     categoryTag() {
       return this.price.category_tag
     },
@@ -159,7 +125,6 @@ export default {
   },
   methods: {
     initPriceCard() {
-      this.priceLocationEmoji = this.getPriceLocationCountryEmoji()
       this.priceOrigin = this.getPriceOriginTag()
       this.priceLabels = this.getPriceLabelsTagsList()
     },
@@ -193,46 +158,6 @@ export default {
         return LabelsTags.filter(lt => this.price.labels_tags.indexOf(lt.id) > -1)
       }
     },
-    getPriceValue(priceValue, priceCurrency) {
-      return priceValue.toLocaleString(navigator.language, {
-        style: 'currency',
-        currency: priceCurrency,
-        minimumFractionDigits: 0,
-        maximumFractionDigits: 2
-      })
-    },
-    getPriceValueDisplay(price) {
-      if (this.hasCategoryTag) {
-        return this.$t('PriceCard.PriceValueDisplay', [this.getPriceValue(price, this.priceCurrency)])
-      }
-      return this.getPriceValue(price, this.priceCurrency)
-    },
-    getPricePerKilo() {
-      const productQuantity = this.price.product.product_quantity
-      let pricePerKilo = (this.priceValue / productQuantity) * 1000
-      return this.$t('PriceCard.PriceValueDisplay', [this.getPriceValue(pricePerKilo, this.priceCurrency)])
-    },
-    getPriceLocationTitle() {
-      if (this.price.location) {
-        return utils.getLocationTitle(this.price.location)
-      }
-      return this.price.location_id
-    },
-    getPriceLocationCountryEmoji() {
-      if (this.price && this.price.location) {
-        return utils.getCountryEmojiFromName(this.price.location.osm_address_country)
-      }
-      return null
-    },
-    getDateFormatted(dateString) {
-      return utils.prettyDate(dateString)
-    },
-    getDateTimeFormatted(dateString) {
-      return utils.prettyDateTime(dateString)
-    },
-    getRelativeDateTimeFormatted(dateTimeString) {
-      return utils.prettyRelativeDateTime(dateTimeString, 'shortest')
-    },
     goToProduct() {
       if (this.readonly) {
         return
@@ -244,18 +169,6 @@ export default {
         return
       }
       this.$router.push({ path: `/brands/${brand}` })
-    },
-    goToLocation() {
-      if (this.readonly) {
-        return
-      }
-      this.$router.push({ path: `/locations/${this.price.location_id}` })
-    },
-    goToUser() {
-      if (this.readonly) {
-        return
-      }
-      this.$router.push({ path: `/users/${this.price.owner}` })
     },
   },
 }

src/components/PricePrice.vue
@@ -1,0 +1,76 @@
+<template>
+  <p>
+    <span class="mr-1">{{ getPriceValueDisplay(priceValue) }}</span>
+    <span v-if="hasProductQuantity" class="mr-1">({{ getPricePerKilo() }})</span>
+    <span v-if="priceWithoutDiscountValue">
+      <v-chip class="mr-1" color="red" variant="outlined" size="small" density="comfortable">
+        {{ $t('PriceCard.Discount') }}
+        <v-tooltip activator="parent" location="top">{{ $t('PriceCard.FullPrice') }} {{ getPriceValueDisplay(priceWithoutDiscountValue) }}</v-tooltip>
+      </v-chip>
+    </span>
+    <i18n-t v-if="!hidePriceDate" keypath="PriceCard.PriceDate" tag="span">
+      <template v-slot:date>
+        <i>{{ getDateFormatted(price.date) }}</i>
+      </template>
+    </i18n-t>
+  </p>
+</template>
+
+<script>
+import utils from '../utils.js'
+
+export default {
+  props: {
+    'price': null,
+    'productQuantity': null,
+    'hidePriceDate': false
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    categoryTag() {
+      return this.price.category_tag
+    },
+    hasCategoryTag() {
+      return !!this.categoryTag
+    },
+    hasProductQuantity() {
+      !!this.productQuantity
+    },
+    priceValue() {
+      return parseFloat(this.price.price)
+    },
+    priceWithoutDiscountValue() {
+      return parseFloat(this.price.price_without_discount)
+    },
+    priceCurrency() {
+      return this.price.currency
+    },
+  },
+  methods: {
+    getPriceValue(priceValue, priceCurrency) {
+      return priceValue.toLocaleString(navigator.language, {
+        style: 'currency',
+        currency: priceCurrency,
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 2
+      })
+    },
+    getPriceValueDisplay(price) {
+      if (this.hasCategoryTag) {
+        return this.$t('PriceCard.PriceValueDisplay', [this.getPriceValue(price, this.priceCurrency)])
+      }
+      return this.getPriceValue(price, this.priceCurrency)
+    },
+    getPricePerKilo() {
+      const productQuantity = this.price.product.product_quantity
+      let pricePerKilo = (this.priceValue / productQuantity) * 1000
+      return this.$t('PriceCard.PriceValueDisplay', [this.getPriceValue(pricePerKilo, this.priceCurrency)])
+    },
+    getDateFormatted(dateString) {
+      return utils.prettyDate(dateString)
+    },
+  }
+}
+</script>

src/components/PriceFooter.vue
@@ -1,0 +1,73 @@
+<template>
+  <div class="d-flex flex-wrap ga-1">
+    <v-chip v-if="!hidePriceLocation" class="mr-1" label size="small" density="comfortable" @click="goToLocation()">
+      <v-icon start icon="mdi-map-marker-outline"></v-icon>
+      {{ getPriceLocationTitle() }}
+      <span v-if="priceLocationEmoji" style="margin-inline-start:5px">{{ priceLocationEmoji }}</span>
+    </v-chip>
+    <v-chip class="mr-1" label size="small" density="comfortable" @click="goToUser()">
+      <v-icon start icon="mdi-account"></v-icon>
+      {{ price.owner }}
+    </v-chip>
+    <v-chip label size="small" density="comfortable">
+      <v-icon start icon="mdi-clock-outline"></v-icon>
+      {{ getRelativeDateTimeFormatted(price.created) }}
+      <v-tooltip activator="parent" location="top">{{ getDateTimeFormatted(price.created) }}</v-tooltip>
+    </v-chip>
+  </div>
+</template>
+
+<script>
+import utils from '../utils.js'
+
+export default {
+  props: {
+    'price': null,
+    'hidePriceLocation': false,
+    'readonly': false
+  },
+  data() {
+    return {
+      priceLocationEmoji: null
+    }
+  },
+  mounted() {
+    this.initPriceFooter()
+  },
+  methods: {
+    initPriceFooter() {
+      this.priceLocationEmoji = this.getPriceLocationCountryEmoji()
+    },
+    getPriceLocationTitle() {
+      if (this.price.location) {
+        return utils.getLocationTitle(this.price.location)
+      }
+      return this.price.location_id
+    },
+    getPriceLocationCountryEmoji() {
+      if (this.price && this.price.location) {
+        return utils.getCountryEmojiFromName(this.price.location.osm_address_country)
+      }
+      return null
+    },
+    getDateTimeFormatted(dateString) {
+      return utils.prettyDateTime(dateString)
+    },
+    getRelativeDateTimeFormatted(dateTimeString) {
+      return utils.prettyRelativeDateTime(dateTimeString, 'shortest')
+    },
+    goToLocation() {
+      if (this.readonly) {
+        return
+      }
+      this.$router.push({ path: `/locations/${this.price.location_id}` })
+    },
+    goToUser() {
+      if (this.readonly) {
+        return
+      }
+      this.$router.push({ path: `/users/${this.price.owner}` })
+    },
+  }
+}
+</script>

src/components/ProductCard.vue
@@ -33,14 +33,29 @@
           </p>
         </v-col>
       </v-row>
+
+      <v-sheet v-if="latestPrice">
+        <v-divider class="mt-2 mb-2"></v-divider>
+        <h4>{{ $t('ProductCard.LatestPrice') }}</h4>
+        <PricePrice :price="latestPrice" :productQuantity="product.product_quantity"></PricePrice>
+        <PriceFooter :price="latestPrice"></PriceFooter>
+      </v-sheet>
     </v-container>
   </v-card>
 </template>
 
 <script>
+import PricePrice from '../components/PricePrice.vue'
+import PriceFooter from '../components/PriceFooter.vue'
+
 export default {
+  components: {
+    PricePrice,
+    PriceFooter,
+  },
   props: {
     'product': null,
+    'latestPrice': null,
     'readonly': false
   },
   data() {

src/router.js
@@ -7,6 +7,7 @@ import UserSettings from './views/UserSettings.vue'
 import AddPriceHome from './views/AddPriceHome.vue'
 import AddPriceSingle from './views/AddPriceSingle.vue'
 import AddPriceMultiple from './views/AddPriceMultiple.vue'
+import Search from './views/Search.vue'
 import PriceList from './views/PriceList.vue'
 import ProductList from './views/ProductList.vue'
 import ProductDetail from './views/ProductDetail.vue'
@@ -21,7 +22,6 @@ import localeManager from './i18n/localeManager.js'
 
 /** @type {import('vue-router').RouterOptions['routes']} */
 const routes = [
-
   { path: '/', name: 'home', component: Home, meta: { title: 'Home', icon: 'mdi-home', drawerMenu: true } },
   { path: '/sign-in', name: 'sign-in', component: SignIn, meta: { title: 'Sign in', icon: 'mdi-login', drawerMenu: true, requiresAuth: false } },
   { path: '/dashboard', name: 'dashboard', component: UserDashboard, meta: { title: 'Dashboard', requiresAuth: true } },
@@ -30,6 +30,7 @@ const routes = [
   { path: '/add/single', name: 'add-price-single', component: AddPriceSingle, meta: { title: 'Add a single price (price tag)', requiresAuth: true }},
   { path: '/add/multiple/price-tag', name: 'add-price-multiple-price-tag', component: AddPriceMultiple, meta: { title: 'Add multiple prices (price tags)', requiresAuth: true }},
   { path: '/add/multiple/receipt', name: 'add-price-multiple-receipt', component: AddPriceMultiple, meta: { title: 'Add multiple prices (receipt)', requiresAuth: true }},
+  { path: '/search', name: 'search', component: Search, meta: { title: 'Search', icon: 'mdi-magnify', drawerMenu: true }},
   { path: '/prices', name: 'prices', component: PriceList, meta: { title: 'LatestPrices', icon: 'mdi-tag-multiple-outline', drawerMenu: true }},
   { path: '/products', name: 'products', component: ProductList, meta: { title: 'TopProducts', icon: 'mdi-database-outline', drawerMenu: true }},
   { path: '/products/:id', name: 'product-detail', component: ProductDetail, meta: { title: 'Product detail' }},

src/views/AddPriceSingle.vue
@@ -37,7 +37,7 @@
                 type="text"
                 hint="EAN"
                 hide-details="auto"
-                @click:prepend="showBarcodeScanner"
+                @click:prepend-inner="showBarcodeScanner"
               ></v-text-field>
               <ProductCard v-if="product" class="mb-4" :product="product" :readonly="true" elevation="1"></ProductCard>
             </v-sheet>

src/views/Home.vue
@@ -2,6 +2,7 @@
   <h1 class="mb-1">{{ $t('Home.Welcome.Title') }}</h1>
   <p>{{ $t('Home.Welcome.Subtitle') }}</p>
   <br />
+  <v-btn class="mb-2" prepend-icon="mdi-magnify" to="/search">{{ $t('Home.SearchProduct') }}</v-btn>
   <v-btn color="primary" prepend-icon="mdi-plus" to="/add">{{ $t('Home.AddPrice') }}</v-btn>
 
   <v-snackbar

src/views/Search.vue
@@ -1,0 +1,116 @@
+<template>
+  <h1 class="mb-1">
+    {{ $t('Search.Title') }}
+    <v-progress-circular v-if="loading" indeterminate :size="30"></v-progress-circular>
+  </h1>
+
+  <v-row>
+    <v-col>
+      <v-form @submit.prevent="search">
+        <v-text-field
+          v-model="productSearchForm.q"
+          :label="$t('Search.SearchByProductBarcode')"
+          type="number"
+          :prepend-inner-icon="formFilled ? 'mdi-barcode' : 'mdi-barcode-scan'"
+          append-inner-icon="mdi-magnify"
+          @click:prepend-inner="showBarcodeScanner"
+          @click:append-inner="search"
+          :rules="[fieldRequired]"
+          :loading="loading"
+          required>
+        </v-text-field>
+      </v-form>
+    </v-col>
+  </v-row>
+
+  <p v-if="productTotal === 0" class="text-red">
+    <i>{{ $t('ProductDetail.ProductNotFound') }}</i>
+  </p>
+
+  <v-row v-if="productTotal > 0" class="mt-0">
+    <v-col cols="12" sm="6" md="4" v-for="product in productList" :key="product">
+      <ProductCard :product="product" :latestPrice="product.latest_price" elevation="1" height="100%"></ProductCard>
+    </v-col>
+  </v-row>
+
+  <BarcodeScanner
+    v-if="barcodeScanner"
+    v-model="barcodeScanner"
+    @barcode="setProductCode($event)"
+    @close="barcodeScanner = false"
+  ></BarcodeScanner>
+</template>
+
+<script>
+import api from '../services/api'
+import BarcodeScanner from '../components/BarcodeScanner.vue'
+import ProductCard from '../components/ProductCard.vue'
+
+export default {
+  components: {
+    BarcodeScanner,
+    ProductCard,
+  },
+  data() {
+    return {
+      productSearchForm: {
+        q: ''
+      },
+      productList: [],
+      productTotal: null,
+      loading: false,
+      // barcode scanner
+      barcodeScanner: false,
+    }
+  },
+  computed: {
+    formFilled() {
+      return Object.values(this.productSearchForm).every(x => !!x)
+    }
+  },
+  methods: {
+    fieldRequired(v) {
+      return !!v
+    },
+    showBarcodeScanner() {
+      this.barcodeScanner = true
+    },
+    setProductCode(code) {
+      this.productSearchForm.q = code
+      this.search()
+    },
+    search() {
+      this.productList = []
+      this.productTotal = null
+      this.getProducts()
+    },
+    getProducts() {
+      this.loading = true
+      return api.getProducts({ code: this.productSearchForm.q })
+        .then((data) => {
+          this.productList.push(...data.items)
+          this.productTotal = data.total
+          this.loading = false
+          if (data.items.length) {
+            this.getProductLatestPrices()
+          }
+        })
+    },
+    getProductLatestPrices() {
+      this.productList.forEach((product, index) => {
+        if (product.price_count && !product.latest_price) {
+          this.getPrices(product)
+        }
+      })
+    },
+    getPrices(product) {
+      return api.getPrices({ product_code: product.code, size: 1, order_by: '-date' })
+        .then((data) => {
+          if (data.items.length) {
+            product.latest_price = data.items[0]
+          }
+        })
+    }
+  }
+}
+</script>

commit 27af69663aab9d8042d9965486db31756623ee29

fix(price card): fix price per kilo display. broken in #195

src/components/PriceCard.vue
@@ -33,7 +33,7 @@
             </span>
           </p>
 
-          <PricePrice v-if="price" :price="price" :productQuantity="product.product_quantity" :hidePriceDate="hidePriceDate"></PricePrice>
+          <PricePrice v-if="price" :price="price" :productQuantity="product ? product.product_quantity : null" :hidePriceDate="hidePriceDate"></PricePrice>
         </v-col>
       </v-row>
 

src/components/PricePrice.vue
@@ -36,7 +36,7 @@ export default {
       return !!this.categoryTag
     },
     hasProductQuantity() {
-      !!this.productQuantity
+      return !!this.productQuantity
     },
     priceValue() {
       return parseFloat(this.price.price)
@@ -64,7 +64,7 @@ export default {
       return this.getPriceValue(price, this.priceCurrency)
     },
     getPricePerKilo() {
-      const productQuantity = this.price.product.product_quantity
+      const productQuantity = this.productQuantity
       let pricePerKilo = (this.priceValue / productQuantity) * 1000
       return this.$t('PriceCard.PriceValueDisplay', [this.getPriceValue(pricePerKilo, this.priceCurrency)])
     },