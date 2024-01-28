f9a5662e999dcfc3652c311e4231fcf60324ceec
Raphael Odini <raphodn@users.noreply.github.com>
2024-01-28 18:31:28 +0100

    New component OpenFoodFactsButton

---
+++ src/components/OpenFoodFactsButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-btn size="small" append-icon="mdi-open-in-new" :href="getOFFUrl()" target="_blank">
+    {{ OFF_NAME }}
+  </v-btn>
+</template>
+
+<script>
+import constants from '../constants'
+
+export default {
+  props: {
+    type: null,
+    value: null,
+  },
+  data () {
+    return {
+      OFF_NAME: constants.OFF_NAME,
+      OFF_URL: constants.OFF_URL,
+    }
+  },
+  methods: {
+    getOFFUrl() {
+      return `${this.OFF_URL}/${this.type}/${this.value}`
+    },
+  }  
+}
+</script>
