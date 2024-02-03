7ed1ab39491b79154208b324936392e84f926951
Raphael Odini <raphodn@users.noreply.github.com>
2024-02-03 14:12:22 +0100

    Add extra highway & building types

--- src/constants.js
+++ src/constants.js
@@ -11,12 +11,15 @@ export default {
     { key: '-price_count', value: 'Number of prices', icon: 'mdi-tag-multiple-outline' },
   ],
   // https://wiki.openstreetmap.org/wiki/Key:place
-  // 'farm'
+  // https://wiki.openstreetmap.org/wiki/Key:highway
+  // https://wiki.openstreetmap.org/wiki/Buildings
   NOMINATIM_RESULT_TYPE_EXCLUDE_LIST: [
     'country', 'state', 'region', 'province', 'district', 'county', 'municipality', 'city', 'borough', 'suburb', 'quarter', 'neighbourhood', 'block', 'city_block', 'plot', 'town', 'village', 'hamlet', 'isolated_dwelling', 'allotments',
     'continent', 'archipelago', 'island', 'islet', 'square', 'locality', 'polder', 'sea', 'ocean',
     'administrative', 'state_district',
-    'residential', 'pedestrian', 'car_sharing', 'primary', 'secondary', 'tertiary',
+    'motorway', 'trunk', 'primary', 'secondary', 'tertiary', 'unclassified', 'residential', 'living_street', 'service', 'pedestrian', 'track', 'road', 'footway',
+    'apartments', 'barracks', 'bungalow', 'cabin', 'detached', 'dormitory', 'ger', 'house', 'houseboat', 'residential',  // 'farm', 'hotel'
     'fuel', 'gas', 'casino', 'parking', 'parking_space', 'charging_station', 'atm',
+    'car_sharing',
   ],
 }
