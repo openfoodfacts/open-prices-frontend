1d5163d8d617f173aae6378ac3cbfbfb3cd10833
dq18 <21193194+dq18@users.noreply.github.com>
2024-02-20 10:27:24 +0100

    feat: add price filter (#358)
    * feat: add price filter
    
    * fix: translation and review suggestion
    
    * fix: uniformity for date toISOstring
    
    ---------
    
    Co-authored-by: dq18 <>

--- src/views/LocationDetail.vue
+++ src/views/LocationDetail.vue
@@ -31,6 +31,32 @@
     <v-progress-circular v-if="loading" indeterminate :size="30"></v-progress-circular>
   </h2>
 
+  <v-row v-if="!loading">
+    <v-col>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn v-bind="props" size="small" class="mr-2" prepend-icon="mdi-filter-variant" :active="!!priceFilter">{{ $t('Common.Filter') }}</v-btn>
+        </template>
+        <v-list>
+          <v-list-item :slim="true" v-for="filter in priceFilterList" :key="filter.key" :prepend-icon="(priceFilter === filter.key) ? 'mdi-check-circle' : 'mdi-circle-outline'" :active="priceFilter === filter.key" @click="togglePriceFilter(filter.key)">
+            {{ filter.value }}
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn v-bind="props" size="small" prepend-icon="mdi-arrow-down" :append-icon="getCurrentPriceOrderIcon" :active="!!priceOrder">{{ $t('Common.Order') }}</v-btn>
+        </template>
+        <v-list>
+          <v-list-item :slim="true" v-for="order in priceOrderList" :key="order.key" :prepend-icon="order.icon" :active="priceOrder === order.key" @click="selectPriceOrder(order.key)">
+            {{ order.value }}
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-col>
+  </v-row>
+
   <v-row>
     <v-col cols="12" sm="6" md="4" v-for="price in locationPriceList" :key="price">
       <PriceCard :price="price" :product="price.product" :hidePriceLocation="true" elevation="1" height="100%"></PriceCard>
@@ -48,6 +74,7 @@
 import api from '../services/api'
 import utils from '../utils.js'
 import { defineAsyncComponent } from 'vue'
+import constants from '../constants'
 
 export default {
   components: {
@@ -62,13 +89,43 @@ export default {
       locationPriceTotal: null,
       locationPricePage: 0,
       loading: false,
+      // filter & order
+      priceFilter: '',
+      priceFilterList: constants.PRICE_FILTER_LIST,
+      priceOrder: constants.PRICE_ORDER_BY_LIST[1].key,
+      priceOrderList: constants.PRICE_ORDER_BY_LIST,
     }
   },
   mounted() {
+    this.priceFilter = this.$route.query[constants.FILTER_PARAM] || this.priceFilter
+    this.priceOrder = this.$route.query[constants.ORDER_BY_PARAM] || this.priceOrder
     this.getLocation(),
     this.getLocationPrices()
   },
+  computed: {
+    locationId() {
+      return this.$route.params.id
+    },
+    getCurrentPriceOrderIcon() {
+      let currentPriceOrder = this.priceOrderList.find(o => o.key === this.priceOrder)
+      return currentPriceOrder ? currentPriceOrder.icon : ''
+    },
+    getPricesParams() {
+      let defaultParams = { location_id: this.locationId, order_by: this.priceOrder, page: this.locationPricePage }
+      if (this.priceFilter === 'show_last_month') {
+        let oneMonthAgo = new Date()
+        oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1)
+        defaultParams['date__gte'] = oneMonthAgo.toISOString().substring(0, 10)
+      }
+      return defaultParams
+    },
+  },
   methods: {
+    initLocationPrices() {
+      this.locationPriceList = []
+      this.locationPricePage = 0
+      this.getLocationPrices()
+    },
     getLocation() {
       return api.getLocationById(this.$route.params.id)
         .then((data) => {
@@ -80,7 +137,7 @@ export default {
     getLocationPrices() {
       this.loading = true
       this.locationPricePage += 1
-      return api.getPrices({ location_id: this.$route.params.id, page: this.locationPricePage })
+      return api.getPrices(this.getPricesParams)
         .then((data) => {
           this.locationPriceList.push(...data.items)
           this.locationPriceTotal = data.total
@@ -95,6 +152,38 @@ export default {
     },
     getLocationOSMUrl(location) {
       return `https://www.openstreetmap.org/${location.osm_type.toLowerCase()}/${location.osm_id}`
+    },
+    togglePriceFilter(filterKey) {
+      this.priceFilter = this.priceFilter ? '' : filterKey
+      this.$router.push({ query: { ...this.$route.query, [constants.FILTER_PARAM]: this.priceFilter } })
+      // this.initLocationPrices() will be called in watch $route
+    },
+    selectPriceOrder(orderKey) {
+      if (this.priceOrder !== orderKey) {
+        this.priceOrder = orderKey
+        this.$router.push({ query: { ...this.$route.query, [constants.ORDER_BY_PARAM]: this.priceOrder } })
+        // this.initLocationPrices() will be called in watch $route
+      }
+    }
+  },
+  watch: {
+    $route (newRoute, oldRoute) {  // only called when query changes to avoid having an API call when the path changes
+      if (oldRoute.path === newRoute.path && JSON.stringify(oldRoute.query) !== JSON.stringify(newRoute.query)) {
+        this.initLocationPrices()
+      }
+      let date = new Date().toISOString();
+
+      console.time('split');
+      for (let i = 0; i < 1000000; i++) {
+        let result = date.split('T')[0];
+      }
+      console.timeEnd('split');
+
+      console.time('substring');
+      for (let i = 0; i < 1000000; i++) {
+        let result = date.substring(0, 10);
+      }
+      console.timeEnd('substring');
     }
   }
 }
