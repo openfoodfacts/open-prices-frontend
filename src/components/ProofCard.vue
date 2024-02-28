af3a1c849f04107360e84c15cb09d3c84ab3fe48
Raphael Odini <raphodn@users.noreply.github.com>
2024-02-28 16:57:54 +0100

    ProofCard: new props hideProofHeader. Emit on close

--- src/components/ProofCard.vue
+++ src/components/ProofCard.vue
@@ -1,12 +1,20 @@
 <template>
   <v-card :id="'proof_' + proof.id" @click="selectProof">
+    <v-card-title v-if="!hideProofHeader">
+      {{ $t('ProofCard.Proof') }} <v-btn style="float:right;" variant="text" density="compact" icon="mdi-close" @click="close"></v-btn>
+    </v-card-title>
+
+    <v-divider v-if="!hideProofHeader"></v-divider>
+
     <v-card-text>
       <v-img :src="getProofUrl(proof)"></v-img>
     </v-card-text>
+
     <v-divider></v-divider>
-    <v-card-text>
+
+    <v-card-actions>
       <ProofFooter :proof="proof" :hideProofDelete="hideProofDelete" :readonly="readonly"></ProofFooter>
-    </v-card-text>
+    </v-card-actions>
   </v-card>
 </template>
 
@@ -19,6 +27,10 @@ export default {
   },
   props: {
     'proof': null,
+    hideProofHeader: {
+      type: Boolean,
+      default: false,
+    },
     hideProofDelete: {
       type: Boolean,
       default: false,
@@ -46,6 +58,9 @@ export default {
       // return 'https://prices.openfoodfacts.org/img/0002/qU59gK8PQw.webp'
       // return 'https://prices.openfoodfacts.net/img/0001/lZGFga9ZOT.webp'
       return `${import.meta.env.VITE_OPEN_PRICES_APP_URL}/img/${proof.file_path}`
+    },
+    close() {
+      this.$emit('close')
     }
   }
 }
