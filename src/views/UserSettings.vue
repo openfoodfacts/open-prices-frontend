5e6c9e9153577675c7cb9133d1d3d9be65e172fc
dq18 <>
2024-03-05 23:34:15 +0100

    feat: select multiple currencies in Settings

--- src/views/UserSettings.vue
+++ src/views/UserSettings.vue
@@ -21,7 +21,7 @@
               :label="$t('UserSettings.LanguageLabel')"
               :items="languageList"
               item-title="native"
-              item-value="code"
+              return-object
               hide-details="auto"
             ></v-autocomplete>
 
@@ -45,11 +45,15 @@
         <v-card :title="$t('UserSettings.AddingPrices')" prepend-icon="mdi-tag-plus-outline">
           <v-divider></v-divider>
           <v-card-text>
-            <h3 class="mb-1">{{ $t('UserSettings.DefaultCurrency') }}</h3>
+            <h3 class="mb-1">{{ $t('UserSettings.FavoriteCurrencies') }}</h3>
             <v-autocomplete
-              v-model="userSettingsForm.currency"
+              v-model="userSettingsForm.selectedCurrencies"
               :label="$t('UserSettings.CurrencyLabel')"
               :items="currencyList"
+              :rules="[v => !!(v && v.length) || $t('UserSettings.CurrencyRequired')]"
+              chips
+              closable-chips
+              multiple
               hide-details="auto"
             ></v-autocomplete>
           </v-card-text>
@@ -79,7 +83,7 @@ export default {
       userSettingsForm: {
         selectedCountry: null, // see initUserSettingsForm
         selectedLanguage: null, // see initUserSettingsForm
-        currency: null,  // see initUserSettingsForm
+        selectedCurrencies: null, // see initUserSettingsForm
       },
       currencyList: [],
       languageList: [],
@@ -91,7 +95,7 @@ export default {
   watch:{
     'userSettingsForm.selectedLanguage': async function () {
       if (this.userSettingsForm.selectedLanguage !== null) {
-        this.languageTranslationCompletion = await localeManager.calculateTranslationCompletion(this.userSettingsForm.selectedLanguage)
+        this.languageTranslationCompletion = await localeManager.calculateTranslationCompletion(this.userSettingsForm.selectedLanguage.code)
       }
     },
     'userSettingsForm.selectedCountry': function (newValue, oldValue) {
@@ -99,6 +103,7 @@ export default {
         const selectedCountry = this.countryList.find(country => country.code === newValue)
         // Update the language list to show the previously selected language first then the selected country languages and the rest of the languages
         let newLanguageList = this.languageList.slice()
+        let newCurrencyList = this.currencyList.slice()
         if (selectedCountry) {
           const currentLanguage = this.languageList.find(lang => lang.code === this.userSettingsForm.selectedLanguage)
           // get the languages of the selected country minus the selected language (if it exists in the country languages list)
@@ -114,6 +119,16 @@ export default {
               !countryLanguagesList.includes(language))
           ].filter(Boolean); // Remove any null or undefined values 
           this.languageList = newLanguageList
+
+          // get the currencies of the selected country minus the already selected currencies (if they exists in the country currencies list)
+          const countryCurrenciesList = selectedCountry.currency.filter(currency => !this.userSettingsForm.selectedCurrencies.includes(currency))
+          // Update the currency list to show the current selected currencies first then the selected country currencies and the rest of the currencies
+          newCurrencyList = [
+            ...this.userSettingsForm.selectedCurrencies,
+            ...countryCurrenciesList,
+            ...newCurrencyList.filter(currency => !selectedCountry.currency.includes(currency) && !this.userSettingsForm.selectedCurrencies.includes(currency))
+          ].filter(Boolean); // Remove any null or undefined values
+          this.currencyList = newCurrencyList
         }
       }
     }
@@ -121,7 +136,12 @@ export default {
   computed: {
     ...mapStores(useAppStore),
     formFilled() {
-      return Object.values(this.userSettingsForm).every(x => !!x)
+      return Object.values(this.userSettingsForm).every(x => {
+        if (x && Array.isArray(x)) {
+          return x.length > 0
+        }
+        return !!x
+      })
     },
   },
   async mounted() {
@@ -138,15 +158,15 @@ export default {
   },
   methods: {
     initUserSettingsForm() {
-      this.userSettingsForm.currency = this.appStore.user.last_currency_used
       this.userSettingsForm.selectedLanguage = this.languageList.find(lang => lang.code === localeManager.guessDefaultLocale()).code
       this.userSettingsForm.selectedCountry = countryData.find(country => country.code === this.appStore.user.country).code
+      this.userSettingsForm.selectedCurrencies = this.appStore.getUserFavoriteCurrencies
     },
     async updateSettings() {
-      await localeManager.changeLanguage(this.userSettingsForm.selectedLanguage)
-      this.appStore.setLanguage(this.userSettingsForm.selectedLanguage)
+      await localeManager.changeLanguage(this.userSettingsForm.selectedLanguage.code)
+      this.appStore.setLanguage(this.userSettingsForm.selectedLanguage.code)
       this.appStore.setCountry(this.userSettingsForm.selectedCountry)
-      this.appStore.setLastCurrencyUsed(this.userSettingsForm.currency)
+      this.appStore.setFavoriteCurrencies(this.userSettingsForm.selectedCurrencies)
       this.$router.push({ path: '/', query: { settingsSuccess: 'true' } })
     }
   }
