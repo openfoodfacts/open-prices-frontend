9bc58a59e713dc65ecf5805639377c286c484333
Raphael Odini <raphodn@users.noreply.github.com>
2024-03-09 09:52:00 +0100

    refactor(product): if labels missing, don't show dialog (#421)

--- src/components/ProductLabelsDialog.vue
+++ src/components/ProductLabelsDialog.vue
@@ -2,18 +2,15 @@
   <v-dialog>
     <v-card>
       <v-card-title>
-        {{ $t('ProductLabels.Title') }} <v-btn style="float:right;" variant="text" density="compact" icon="mdi-close" @click="close"></v-btn>
+        {{ $t('ProductCard.Labels') }} <v-btn style="float:right;" variant="text" density="compact" icon="mdi-close" @click="close"></v-btn>
       </v-card-title>
 
       <v-divider></v-divider>
 
-      <v-card-text v-if="labels.length">
+      <v-card-text>
         <v-chip v-for="label in labels" :key="label" label class="mr-2 mb-2">
           {{ label }}
         </v-chip>
-      </v-card-text>
-      <v-card-text v-if="!labels.length">
-        <span class="text-red">{{ $t('ProductLabels.Empty') }}</span>
       </v-card-text>
     </v-card>
   </v-dialog>
@@ -22,15 +19,7 @@
 <script>
 export default {
   props: {
-    'labels': Array,
-  },
-  data() {
-    return {
-    }
-  },
-  computed: {
-  },
-  mounted() {
+    labels: Array,
   },
   methods: {
     close() {
