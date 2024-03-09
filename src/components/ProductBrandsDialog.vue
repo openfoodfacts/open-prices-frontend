4a8326c002ad77d85c02be3d64c9807f2982d779
Raphael Odini <raphodn@users.noreply.github.com>
2024-03-09 11:01:34 +0100

    feat(product): if too many brands, show count & dialog (#424)

---
+++ src/components/ProductBrandsDialog.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-dialog>
+    <v-card>
+      <v-card-title>
+        {{ $t('ProductCard.Brands') }} <v-btn style="float:right;" variant="text" density="compact" icon="mdi-close" @click="close"></v-btn>
+      </v-card-title>
+
+      <v-divider></v-divider>
+
+      <v-card-text>
+        <v-chip v-for="brand in brands" :key="brand" label class="mr-2 mb-2" @click="goToBrand(brand)">
+          {{ brand }}
+        </v-chip>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    brands: Array,
+  },
+  methods: {
+    goToBrand(brand) {
+      this.$router.push({ path: `/brands/${brand}` })
+    },
+    close() {
+      this.$emit('close')
+    },
+  }
+}
+</script>
