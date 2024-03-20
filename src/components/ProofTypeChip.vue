6bbea76265ab56e5d1934a039bee01a39e5fa6ba
Raphael Odini <raphodn@users.noreply.github.com>
2024-03-20 13:42:22 +0100

    New ProofTypeChip component

---
+++ src/components/ProofTypeChip.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-chip label size="small" density="comfortable">
+    <v-icon start icon="mdi-paperclip"></v-icon>
+    <span v-if="proof.type === 'GDPR_REQUEST'">
+      <a :href="OFF_WIKI_GDPR_REQUEST_URL" target="_blank">
+        {{ proofTypeName }}
+        <v-icon size="x-small" icon="mdi-open-in-new"></v-icon>
+      </a>
+    </span>
+    <span v-if="proof.type !== 'GDPR_REQUEST'">
+      {{ proofTypeName }}
+    </span>
+  </v-chip>
+</template>
+
+<script>
+import constants from '../constants'
+
+export default {
+  props: {
+    'proof': null,
+  },
+  data() {
+    return {
+      OFF_WIKI_GDPR_REQUEST_URL: constants.OFF_WIKI_GDPR_REQUEST_URL,
+    }
+  },
+  computed: {
+    proofTypeName() {
+      return this.$t(`ProofCard.${this.proof.type}`)
+    }
+  }
+}
+</script>
