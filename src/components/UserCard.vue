d94d6dfcc0578612a27a636619bda01a0ac04842
Raphael Odini <raphodn@users.noreply.github.com>
2024-03-31 21:34:42 +0200

    New UserCard component

---
+++ src/components/UserCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card
+    :title="user.user_id"
+    prepend-icon="mdi-account"
+    @click="goToUser(user)">
+    <v-card-text>
+      <PriceCountChip :count="user.price_count" :withLabel="true"></PriceCountChip>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+export default {
+  components: {
+    'PriceCountChip': defineAsyncComponent(() => import('../components/PriceCountChip.vue')),
+  },
+  props: {
+    user: {
+      type: Object,
+      required: true
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    goToUser(user) {
+      if (this.readonly) {
+        return
+      }
+      this.$router.push({ path: `/users/${user.user_id}` })
+    },
+  }
+}
+</script>
