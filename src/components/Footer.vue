b2e9c4e310f7e10e206f90087d496ac63b52e27c
Raphaël Odini <raphodn@users.noreply.github.com>
2024-04-03 19:34:35 +0200

    feat(Footer): display the list of 4 sources (OFF, OBF, OPF, OPFF) (#503)

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,6 +8,23 @@
             <OpenFoodFactsLink display="link"></OpenFoodFactsLink>
           </template>
         </i18n-t>
+        <br />
+        <v-chip class="mr-1" label size="small" density="comfortable" prepend-icon="mdi-food-outline">
+          {{ $t('Common.Food') }}
+          <v-tooltip activator="parent" open-on-click location="top">{{ OFF_NAME }}</v-tooltip>
+        </v-chip>
+        <v-chip class="mr-1" label size="small" density="comfortable" prepend-icon="mdi-lotion-outline">
+          {{ $t('Common.Beauty') }}
+          <v-tooltip activator="parent" open-on-click location="top">{{ OBF_NAME }}</v-tooltip>
+        </v-chip>
+        <v-chip class="mr-1" label size="small" density="comfortable" prepend-icon="mdi-bookshelf">
+          {{ $t('Common.Products') }}
+          <v-tooltip activator="parent" open-on-click location="top">{{ OPF_NAME }}</v-tooltip>
+        </v-chip>
+        <v-chip label size="small" density="comfortable" prepend-icon="mdi-paw">
+          {{ $t('Common.PetFood') }}
+          <v-tooltip activator="parent" open-on-click location="top">{{ OPFF_NAME }}</v-tooltip>
+        </v-chip>
       </v-col>
       <v-col cols="12" md="6" align="center">
         <v-btn class="mx-2" variant="text" href="https://prices.openfoodfacts.org" target="_blank">{{ $t('Footer.About') }}</v-btn>
@@ -30,6 +47,9 @@ export default {
     return {
       APP_NAME: constants.APP_NAME,
       OFF_NAME: constants.OFF_NAME,
+      OBF_NAME: constants.OBF_NAME,
+      OPFF_NAME: constants.OPFF_NAME,
+      OPF_NAME: constants.OPF_NAME,
     }
   },
 }
