25913c1cc50626ee592e99b4a3801cd7bff46847
Raphaël Odini <raphodn@users.noreply.github.com>
2024-06-06 10:51:29 +0200

    refactor(product): add new categories (breads, viennoiseries, eggs) (#609)

--- data/categories/filter_categories.py
+++ data/categories/filter_categories.py
@@ -1,14 +1,20 @@
+"""
+https://wiki.openfoodfacts.org/Global_categories_taxonomy
+"""
 import os
 import re
 import json
-from openfoodfacts.taxonomy import get_taxonomy
+from openfoodfacts.taxonomy import get_taxonomy, Taxonomy, TaxonomyNode
 
 PARENT_CATEGORIES_ID = [
-    "en:vegetables",  # 391
+    "en:vegetables",  # 391 descendants
     "en:fruits",  # 287
     "en:culinary-plants",  # 152
+    "en:breads",  # 103
     "en:nuts",  # 77
+    "en:viennoiseries",  # 47
     "en:potatoes",  # 27
+    "en:eggs",  # 21
     "en:textured-vegetable-protein"  # 2
 ]
 
@@ -32,13 +38,16 @@ def get_languages():
     with open(os.path.join(script_path, "../../src/i18n/data/languages.json")) as f:
         return json.load(f)
 
-def get_taxonomy_node_list_by_id_list(taxonomy, node_id_list):
+def get_taxonomy_node_list_by_id_list(taxonomy: Taxonomy, node_id_list) -> list[TaxonomyNode]:
     return [node for node in taxonomy.iter_nodes() if node.id in node_id_list]
 
-def get_all_descendants(taxonomy, node_parent):
+def get_all_root_nodes(taxonomy: Taxonomy) -> list[TaxonomyNode]:
+    return [node for node in taxonomy.iter_nodes() if not node.get_parents_hierarchy()]
+
+def get_all_descendants(taxonomy: Taxonomy, node_parent: TaxonomyNode) -> list[TaxonomyNode]:
     return [node for node in taxonomy.iter_nodes() if node_parent in node.get_parents_hierarchy()]
 
-def filter_categories(categories, parent_categories):
+def filter_categories(categories: Taxonomy, parent_categories: list[TaxonomyNode]) -> list[TaxonomyNode]:
     # Get all descendants for every parent node in parent_categories
     all_descendants = [descendant for parent_node in parent_categories for descendant in get_all_descendants(categories, parent_node)]
     # Add extra children to the list of descendants
@@ -49,7 +58,7 @@ def filter_categories(categories, parent_categories):
     filtered_descendants = {node for node in all_descendants if not any(re.search(r'\b{}\b'.format(filter_word), node.get_localized_name("en"), flags=re.IGNORECASE) for filter_word in ADDITIONAL_FILTERING) and node.id.startswith("en:")}
     return filtered_descendants
 
-def write_categories_to_files(categories, delete_parents=False):
+def write_categories_to_files(categories: Taxonomy, delete_parents=False):
     languages = get_languages()
     for language in languages:
         language_code = language['code']
@@ -85,23 +94,35 @@ def compare_new_categories_with_old_categories():
 
     print("missing in old", len(category_missing_in_old_list))
 
+
 if __name__ == "__main__":
     """
     How-to run ?
     > pip install openfoodfacts
     > python filter_categories.py
     """
-    # init
-    CATEGORIES_FULL = get_taxonomy("category")
+    # Step 1a: get all the categories
+    CATEGORIES_FULL: Taxonomy = get_taxonomy("category")
     print("Total number of categories:", len(CATEGORIES_FULL))
-    PARENT_CATEGORIES = get_taxonomy_node_list_by_id_list(CATEGORIES_FULL, PARENT_CATEGORIES_ID)
+
+    # Step 1b: get all the parent categories
+    PARENT_CATEGORIES: list[TaxonomyNode] = get_taxonomy_node_list_by_id_list(CATEGORIES_FULL, PARENT_CATEGORIES_ID)
     print("Filter with the following parent categories:", [node.id for node in PARENT_CATEGORIES])
 
-    categories_filtered = filter_categories(CATEGORIES_FULL, PARENT_CATEGORIES)
+    # Step 2: filter categories
+    categories_filtered: list[TaxonomyNode] = filter_categories(CATEGORIES_FULL, PARENT_CATEGORIES)
     categories_filtered_to_dict_list = [{"id": node.id, **node.to_dict()} for node in categories_filtered]
     print("Categories remaining:", len(categories_filtered_to_dict_list))
 
+    # Step 3: write to files (1 per language)
     write_categories_to_files(categories_filtered_to_dict_list, delete_parents=True)
     print("Wrote to language files")
 
+    # Extra
     # compare_new_categories_with_old_categories()
+    # root_nodes = get_all_root_nodes(CATEGORIES_FULL)
+    # print(root_nodes)
+    # category_name = "en:eggs"
+    # category_descendants = get_all_descendants(CATEGORIES_FULL, get_taxonomy_node_list_by_id_list(CATEGORIES_FULL, [category_name])[0])
+    # print(category_descendants)
+    # print(len(category_descendants))
