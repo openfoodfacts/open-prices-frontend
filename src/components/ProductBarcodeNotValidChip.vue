9cb88720496b845cc0a8dd4ea3938f7a5ab13148
Raphael Odini <raphodn@users.noreply.github.com>
2024-06-09 11:20:00 +0200

    New ProductBarcodeNotValidChip component

---
+++ src/components/ProductBarcodeNotValidChip.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-chip label size="small" density="comfortable" prepend-icon="mdi-help" color="warning" data-name="product-barcode-not-valid-chip">
+    {{ $t('ProductCard.ProductBarcodeNotValid') }}
+  </v-chip>
+</template>
