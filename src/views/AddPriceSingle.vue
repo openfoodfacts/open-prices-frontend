f957a6b54bbfa658d5542063eb62071346733244
Raphael Odini <raphodn@users.noreply.github.com>
2024-06-18 00:58:44 +0200

    Price add: use ProofInputRow in AddPriceSingle

--- src/views/AddPriceSingle.vue
+++ src/views/AddPriceSingle.vue
@@ -50,51 +50,7 @@
             <h3 class="mt-4 mb-1">
               {{ $t('AddPriceSingle.PriceDetails.Proof') }}
             </h3>
-            <v-row>
-              <v-col>
-                <v-btn class="mb-2 mr-2" size="small" prepend-icon="mdi-camera" :loading="createProofLoading" :disabled="createProofLoading" @click.prevent="$refs.proofCamera.click()">
-                  <span class="d-sm-none">{{ $t('AddPriceSingle.PriceDetails.Picture') }}</span>
-                  <span class="d-none d-sm-inline-flex">{{ $t('AddPriceSingle.PriceDetails.TakePicture') }}</span>
-                </v-btn>
-                <v-btn class="mb-2 mr-2" size="small" prepend-icon="mdi-image-plus" :loading="createProofLoading" :disabled="createProofLoading" @click.prevent="$refs.proofGallery.click()">
-                  <span class="d-sm-none">{{ $t('AddPriceSingle.PriceDetails.Gallery') }}</span>
-                  <span class="d-none d-sm-inline-flex">{{ $t('AddPriceSingle.PriceDetails.SelectFromGallery') }}</span>
-                </v-btn>
-                <v-btn class="mb-2" size="small" prepend-icon="mdi-receipt-text-clock" @click="showUserRecentProofs">
-                  <span class="d-sm-none">{{ $t('AddPriceSingle.PriceDetails.RecentProof') }}</span>
-                  <span class="d-none d-sm-inline-flex">{{ $t('AddPriceSingle.PriceDetails.SelectRecentProof') }}</span>
-                </v-btn>
-                <v-file-input
-                  ref="proofCamera"
-                  v-model="proofImage"
-                  class="d-none overflow-hidden"
-                  capture="environment"
-                  accept="image/*"
-                  :loading="createProofLoading"
-                  @change="newProof('camera')"
-                  @click:clear="clearProof"
-                />
-                <v-file-input
-                  ref="proofGallery"
-                  v-model="proofImage"
-                  class="d-none overflow-hidden"
-                  accept="image/*, .heic"
-                  :loading="createProofLoading"
-                  @change="newProof('gallery')"
-                  @click:clear="clearProof"
-                />
-                <p v-if="proofFormFilled && !createProofLoading" class="text-green mt-2 mb-2">
-                  <i v-if="!proofSelectedMessage">{{ $t('AddPriceSingle.PriceDetails.ProofUploaded') }}</i>
-                  <i v-if="proofSelectedMessage">{{ $t('AddPriceSingle.PriceDetails.ProofSelected') }}</i>
-                </p>
-                <p v-if="!proofFormFilled && !createProofLoading" class="text-red mt-2 mb-2">
-                  <i>{{ $t('AddPriceSingle.PriceDetails.UploadProof') }}</i>
-                </p>
-              </v-col>
-              <v-col v-if="proofFormFilled">
-                <v-img :src="proofImagePreview" style="max-height:200px" />
-              </v-col>
-            </v-row>
+            <ProofInputRow :proofType="proofType" :proofForm="addPriceSingleForm" />
           </v-card-text>
         </v-card>
       </v-col>
@@ -171,20 +127,6 @@
   >
     {{ $t('AddPriceSingle.PriceDetails.ProofDateChanged') }}
   </v-snackbar>
-  <v-snackbar
-    v-model="proofSuccessMessage"
-    color="success"
-    :timeout="2000"
-  >
-    {{ $t('AddPriceSingle.PriceDetails.ProofUploaded') }}
-  </v-snackbar>
-  <v-snackbar
-    v-model="proofSelectedSuccessMessage"
-    color="success"
-    :timeout="2000"
-  >
-    {{ $t('AddPriceSingle.PriceDetails.ProofSelected') }}
-  </v-snackbar>
 
   <LocationSelectorDialog
     v-if="locationSelectorDialog"
@@ -192,40 +134,25 @@
     @location="setLocationData($event)"
     @close="locationSelectorDialog = false"
   />
-  <UserRecentProofsDialog
-    v-if="userRecentProofsDialog"
-    v-model="userRecentProofsDialog"
-    @recentProofSelected="handleRecentProofSelected($event)"
-    @close="userRecentProofsDialog = false"
-  />
 </template>
 
 <script>
-import Compressor from 'compressorjs'
-import ExifReader from 'exifreader'
 import { defineAsyncComponent } from 'vue'
 import { mapStores } from 'pinia'
 import { useAppStore } from '../store'
 import api from '../services/api'
 import utils from '../utils.js'
 
-Compressor.setDefaults({
-  checkOrientation: true,  // default
-  retainExif: true,
-  quality: 0.6,
-  mimeType: 'image/webp',
-  maxWidth: 3000
-})
-
 export default {
   components: {
     ProductInputRow: defineAsyncComponent(() => import('../components/ProductInputRow.vue')),
     PriceInputRow: defineAsyncComponent(() => import('../components/PriceInputRow.vue')),
+    ProofInputRow: defineAsyncComponent(() => import('../components/ProofInputRow.vue')),
     LocationSelectorDialog: defineAsyncComponent(() => import('../components/LocationSelectorDialog.vue')),
-    UserRecentProofsDialog: defineAsyncComponent(() => import('../components/UserRecentProofsDialog.vue')),
   },
   data() {
     return {
+      proofType: 'PRICE_TAG',
       // price form
       addPriceSingleForm: {
         mode: '',
@@ -250,14 +177,7 @@ export default {
       // location data
       locationSelectorDialog: false,
       // proof data
-      userRecentProofsDialog: false,
-      proofImage: null,
-      proofImagePreview: null,
-      createProofLoading: false,
       proofDateSuccessMessage: false,
-      proofSuccessMessage: false,
-      proofSelectedSuccessMessage: false,
-      proofSelectedMessage: false,
       categoryPricePerList: [
         {key: 'KILOGRAM', value: this.$t('AddPriceSingle.CategoryPricePer.PerKg'), icon: 'mdi-weight-kilogram'},
         {key: 'UNIT', value: this.$t('AddPriceSingle.CategoryPricePer.PerUnit'), icon: 'mdi-numeric-1-circle'}
@@ -305,8 +225,6 @@ export default {
         this.addPriceSingleForm.mode = 'barcode'
         this.addPriceSingleForm.product_code = this.$route.query.code
       }
-    } else if (this.$route.query.proof_id) {
-      this.getProofById(this.$route.query.proof_id)
     }
     this.initPriceSingleForm()
   },
@@ -323,88 +241,6 @@ export default {
       }
       this.addPriceSingleForm.price_per = this.categoryPricePerList[0].key // init to 'KILOGRAM' because it's the most common use-case
       this.addPriceSingleForm.currency = this.appStore.getUserLastCurrencyUsed
-    },
-    showUserRecentProofs() {
-      this.userRecentProofsDialog = true
-    },
-    handleProofSelected(proof) {
-      this.addPriceSingleForm.proof_id = proof.id
-      this.addPriceSingleForm.date = new Date(proof.created).toISOString().split('T')[0]
-      // this.proofDateSuccessMessage = true
-      this.proofImagePreview = this.getProofUrl(proof)
-      this.proofSelectedSuccessMessage = true
-      this.proofSelectedMessage = true
-    },
-    handleRecentProofSelected(selectedProof) {
-      this.handleProofSelected(selectedProof)
-      this.proofImagePreview = this.getProofUrl(selectedProof)
-    },
-    getProofUrl(proof) {
-      return `${import.meta.env.VITE_OPEN_PRICES_APP_URL}/img/${proof.file_path}`
-    },
-    getProofById(proofId) {
-      this.loading = true
-      api.getProofById(proofId)
-        .then(proof => {
-          this.handleProofSelected(proof)
-          this.loading = false
-        })
-    },
-    newProof(source) {
-      if (source === 'gallery') {
-        ExifReader.load(this.proofImage[0]).then((tags) => {
-          if (tags['DateTimeOriginal'] && tags['DateTimeOriginal'].description) {
-            // exif DateTimeOriginal format: '2024:01:31 20:23:52'
-            const imageDateString = tags['DateTimeOriginal'].description.substring(0, 10).replaceAll(':', '-')
-            if (imageDateString !== this.addPriceSingleForm.date) {
-              this.addPriceSingleForm.date = imageDateString
-              this.proofDateSuccessMessage = true
-            }
-          }
-        })
-      }
-      this.uploadProof()
-    },
-    uploadProof() {
-      this.createProofLoading = true
-      new Promise((resolve, reject) => {
-        new Compressor(this.proofImage[0], {
-          success: resolve,
-          error: reject
-        })
-      })
-      .then((proofImageCompressed) => {
-        api
-          .createProof(proofImageCompressed, 'PRICE_TAG')
-          .then((data) => {
-            this.createProofLoading = false
-            if (data['id']) {
-              this.addPriceSingleForm.proof_id = data['id']
-              this.proofImagePreview = URL.createObjectURL(proofImageCompressed)
-              this.proofSuccessMessage = true
-            } else {
-              alert('Error: server error')
-              console.log(data)
-            }
-          })
-          .catch((error) => {
-            alert('Error: server error')
-            console.log(error)
-            this.createProofLoading = false
-          })
-      })
-      .catch((error) => {
-        alert('Error: compression')
-        console.log(error)
-      })
-      // .finally(() => {
-      //   console.log('Compress complete')
-      // })
-    },
-    clearProof() {
-      this.proofImage = null
-      this.proofImagePreview = null
-      this.addPriceSingleForm.proof_id = null
     },
     showBarcodeScannerDialog() {
       this.barcodeScannerDialog = true
