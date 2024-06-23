677ced770f343077ccffa618f81bff68bddd5869
Raphaël Odini <raphodn@users.noreply.github.com>
2024-06-23 23:54:52 +0200

    feat(price add): hide single price add workflow (#635)

--- src/views/AddPriceHome.vue
+++ src/views/AddPriceHome.vue
@@ -6,21 +6,6 @@
   <v-row>
     <v-col cols="12" sm="6" lg="4">
       <v-card
-        :title="$t('AddPriceHome.SingleProductMode.Title')"
-        :subtitle="$t('AddPriceHome.SingleProductMode.Subtitle')"
-        prepend-icon="mdi-barcode"
-        to="/prices/add/single"
-      />
-    </v-col>
-  </v-row>
-
-  <br>
-  <v-divider />
-  <br>
-
-  <v-row>
-    <v-col cols="12" sm="6" lg="4">
-      <v-card
         :title="$t('AddPriceHome.MultipleProductMode.Title')"
         :subtitle="$t('AddPriceHome.MultipleProductMode.Subtitle')"
         prepend-icon="mdi-library-shelves"
--- src/views/AddPriceSingle.vue
+++ src/views/AddPriceSingle.vue
@@ -64,7 +64,7 @@
                 </v-item>
               </v-item-group>
             </h3>
-            <PriceInputRow :priceForm="addPriceSingleForm" @filled="pricePriceFormFilled = $event" />
+            <PriceInputRow :priceForm="addPriceSingleForm" :hideCurrencyChoice="true" @filled="pricePriceFormFilled = $event" />
             <h3 class="mt-4 mb-1">
               {{ $t('AddPriceSingle.PriceDetails.Proof') }}
             </h3>
