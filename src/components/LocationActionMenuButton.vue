6d7735d552eb34502f1032b84dc01cf0c6d96ecf
Raphaël Odini <raphodn@users.noreply.github.com>
2024-08-14 16:05:41 +0200

    refactor(Location card): new action menu with OSM link (#697)

---
+++ src/components/LocationActionMenuButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-btn :style="style" icon size="small" density="comfortable" variant="text">
+    <v-icon>mdi-dots-vertical</v-icon>
+    <v-menu activator="parent" scroll-strategy="close" transition="slide-y-transition">
+      <v-list>
+        <OpenStreetMapLink :location="location" display="list-item" />
+      </v-list>
+    </v-menu>
+  </v-btn>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+export default {
+  components: {
+    OpenStreetMapLink: defineAsyncComponent(() => import('../components/OpenStreetMapLink.vue'))
+  },
+  props: {
+    location: {
+      type: Object,
+      default: null
+    },
+    style: {
+      type: String,
+      default: 'position:absolute;bottom:6px;right:0;'
+    }
+  }
+}
+</script>
