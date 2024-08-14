f5cf1886d4b97a8fadc9fdeb014b7c50069564dc
Raphael Odini <raphodn@users.noreply.github.com>
2024-08-14 16:42:54 +0200

    refactor(Product card): new action menu with OFF link

---
+++ src/components/ProductActionMenuButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-btn :style="style" icon size="small" density="comfortable" variant="text">
+    <v-icon>mdi-dots-vertical</v-icon>
+    <v-menu activator="parent" scroll-strategy="close" transition="slide-y-transition">
+      <v-list>
+        <OpenFoodFactsLink display="list-item" :source="product.source" facet="product" :value="product.code" />
+      </v-list>
+    </v-menu>
+  </v-btn>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+export default {
+  components: {
+    OpenFoodFactsLink: defineAsyncComponent(() => import('../components/OpenFoodFactsLink.vue'))
+  },
+  props: {
+    product: {
+      type: Object,
+      default: null
+    },
+    style: {
+      type: String,
+      default: 'position:absolute;bottom:6px;right:0;'
+    }
+  }
+}
+</script>
