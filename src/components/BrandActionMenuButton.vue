d698fedaa61cee3ed1609c19b41b03cf3167d934
Raphael Odini <raphodn@users.noreply.github.com>
2024-08-17 16:10:59 +0200

    New BrandActionMenuButton component

---
+++ src/components/BrandActionMenuButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-btn :style="style" icon size="small" density="comfortable" variant="text">
+    <v-icon>mdi-dots-vertical</v-icon>
+    <v-menu activator="parent" scroll-strategy="close" transition="slide-y-transition">
+      <v-list>
+        <ShareLink :overrideUrl="getShareLinkUrl" display="list-item" />
+        <v-divider />
+        <OpenFoodFactsLink facet="brand" :value="brand" display="list-item" />
+      </v-list>
+    </v-menu>
+  </v-btn>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+export default {
+  components: {
+    ShareLink: defineAsyncComponent(() => import('../components/ShareLink.vue')),
+    OpenFoodFactsLink: defineAsyncComponent(() => import('../components/OpenFoodFactsLink.vue'))
+  },
+  props: {
+    brand: {
+      type: String,
+      default: null
+    },
+    style: {
+      type: String,
+      default: 'position:absolute;bottom:6px;right:0;'
+    }
+  },
+  computed: {
+    getShareLinkUrl() {
+      return `/brands/${this.brand}`
+    }
+  }
+}
+</script>
