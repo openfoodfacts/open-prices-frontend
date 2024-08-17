db1f0d4c722cebe1c9ee3a192770e54b40f5bf6d
Raphael Odini <raphodn@users.noreply.github.com>
2024-08-17 16:44:24 +0200

    new ProductCountChip component

---
+++ src/components/ProductCountChip.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-chip label size="small" density="comfortable" class="mr-1">
+    <v-icon start icon="mdi-food-outline" />
+    <span v-if="withLabel" id="product-count">{{ $t('Common.ProductCount', { count: count }) }}</span>
+    <span v-else id="product-count">{{ count }}</span>
+  </v-chip>
+</template>
+
+<script>
+export default {
+  props: {
+    count: {
+      type: Number,
+      default: null
+    },
+    withLabel: {
+      type: Boolean,
+      default: false
+    },
+  }
+}
+</script>
