caa52617efb8aa080d8aa129b15fe59940a5adf3
Raphael Odini <raphodn@users.noreply.github.com>
2024-08-17 15:51:18 +0200

    refactor(Brand detail): new BrandCard component

---
+++ src/components/BrandCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card :title="brand" prepend-icon="mdi-factory" data-name="brand-card">
+    <v-card-text>
+      <v-chip label size="small" density="comfortable" class="mr-1">
+        <v-icon start icon="mdi-food-outline" />
+        {{ $t('BrandDetail.BrandProductTotal', { count: productCount }) }}
+      </v-chip>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    brand: {
+      type: String,
+      default: null
+    },
+    productCount: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
