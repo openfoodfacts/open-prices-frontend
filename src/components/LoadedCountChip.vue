41d4f1b557657aa9e7987d0deb5b20285f9bc17a
Raphael Odini <raphodn@users.noreply.github.com>
2024-08-20 23:56:49 +0200

    New LoadedCountChip component

---
+++ src/components/LoadedCountChip.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-chip
+    class="mr-2"
+    variant="tonal"
+    size="x-small"
+  >
+    <span v-if="totalCount">
+      {{ loadedCount }}&nbsp;/&nbsp;{{ totalCount }}
+    </span>
+    <span v-else>
+      {{ totalCount }}
+    </span>
+  </v-chip>
+</template>
+
+<script>
+export default {
+  props: {
+    loadedCount: {
+      type: Number,
+      default: 0
+    },
+    totalCount: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
