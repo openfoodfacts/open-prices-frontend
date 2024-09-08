a468742e3ba81204ca6cefb366bf66fb34590bef
Raphael Odini <raphodn@users.noreply.github.com>
2024-09-08 19:39:40 +0200

    PriceFooterRow: remove User & Created info

--- src/components/PriceFooterRow.vue
+++ src/components/PriceFooterRow.vue
@@ -2,10 +2,8 @@
   <v-row style="margin-top:0;">
     <v-col cols="11">
       <LocationChip v-if="!hidePriceLocation" class="mr-1" :location="price.location" :locationId="price.location_id" :readonly="readonly" />
-      <UserChip class="mr-1" :username="price.owner" :readonly="readonly" />
       <DateChip class="mr-1" :date="price.date" :readonly="readonly" />
       <ProofChip v-if="price.proof && !hidePriceProof" class="mr-1" :proof="price.proof" />
-      <RelativeDateTimeChip :dateTime="price.created" />
     </v-col>
   </v-row>
 
@@ -18,10 +16,8 @@ import { defineAsyncComponent } from 'vue'
 export default {
   components: {
     LocationChip: defineAsyncComponent(() => import('../components/LocationChip.vue')),
-    UserChip: defineAsyncComponent(() => import('../components/UserChip.vue')),
     DateChip: defineAsyncComponent(() => import('../components/DateChip.vue')),
     ProofChip: defineAsyncComponent(() => import('../components/ProofChip.vue')),
-    RelativeDateTimeChip: defineAsyncComponent(() => import('../components/RelativeDateTimeChip.vue')),
     PriceActionMenuButton: defineAsyncComponent(() => import('../components/PriceActionMenuButton.vue')),
   },
   props: {
