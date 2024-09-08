8741e1a0961d7ce8a63c1007046831a2ba5dc904
Raphael Odini <raphodn@users.noreply.github.com>
2024-09-08 19:37:47 +0200

    New HistoryCard to display price created metadata

---
+++ src/components/HistoryCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card :title="$t('Common.History')" data-name="history-card">
+    <v-card-text>
+      {{ getDateTimeFormatted(price.created) }} ({{ getRelativeDateTimeFormatted(price.created) }})
+      <span>- </span>
+      <a :href="getUserDetailUrl">{{ price.owner }}</a>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import utils from '../utils.js'
+
+export default {
+  props: {
+    price: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    getUserDetailUrl() {
+      return `/users/${this.price.owner}`
+    }
+  },
+  methods: {
+    getDateTimeFormatted(dateTimeString) {
+      return utils.offDateTime(dateTimeString)
+    },
+    getRelativeDateTimeFormatted(dateTimeString) {
+      return utils.prettyRelativeDateTime(dateTimeString, 'short')
+    },
+  }
+}
+</script>
