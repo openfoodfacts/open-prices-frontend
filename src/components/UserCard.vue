cbf85c48e350ba99b964d9ecb513ed37fa11f9b4
Raphael Odini <raphodn@users.noreply.github.com>
2024-09-12 22:08:26 +0200

    Fix display in User detail page

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -7,15 +7,15 @@
   >
     <v-card-text>
       <PriceCountChip :count="user.price_count" :withLabel="true" />
-      <v-chip label size="small" density="comfortable" class="mr-1">
+      <v-chip v-if="user.location_count" label size="small" density="comfortable" class="mr-1">
         <v-icon start icon="mdi-map-marker-outline" />
         <span id="product-count">{{ $t('Common.LocationCount', { count: user.location_count }) }}</span>
       </v-chip>
-      <v-chip label size="small" density="comfortable" class="mr-1">
+      <v-chip v-if="user.product_count" label size="small" density="comfortable" class="mr-1">
         <v-icon start icon="mdi-database-outline" />
         <span id="product-count">{{ $t('Common.ProductCount', { count: user.product_count }) }}</span>
       </v-chip>
-      <v-chip label size="small" density="comfortable">
+      <v-chip v-if="user.proof_count" label size="small" density="comfortable">
         <v-icon start icon="mdi-image" />
         <span id="product-count">{{ $t('Common.ProofCount', { count: user.proof_count }) }}</span>
       </v-chip>
