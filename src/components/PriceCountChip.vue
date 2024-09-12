62cb542b6548bddfc267446a6faaad2735658224
Raphaël Odini <raphodn@users.noreply.github.com>
2024-09-12 22:10:43 +0200

    feat(users): show location, product & proof counts (#809)

--- src/components/PriceCountChip.vue
+++ src/components/PriceCountChip.vue
@@ -1,7 +1,7 @@
 <template>
   <v-chip label size="small" density="comfortable" :color="getColor()" class="mr-1">
     <v-icon start icon="mdi-tag-outline" />
-    <span v-if="withLabel" id="price-count">{{ $t('PriceCountChip.PriceCount', { count: count }) }}</span>
+    <span v-if="withLabel" id="price-count">{{ $t('Common.PriceCount', { count: count }) }}</span>
     <span v-else id="price-count">{{ count }}</span>
   </v-chip>
 </template>
--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -7,6 +7,18 @@
   >
     <v-card-text>
       <PriceCountChip :count="user.price_count" :withLabel="true" />
+      <v-chip v-if="user.location_count" label size="small" density="comfortable" class="mr-1">
+        <v-icon start icon="mdi-map-marker-outline" />
+        <span id="product-count">{{ $t('Common.LocationCount', { count: user.location_count }) }}</span>
+      </v-chip>
+      <v-chip v-if="user.product_count" label size="small" density="comfortable" class="mr-1">
+        <v-icon start icon="mdi-database-outline" />
+        <span id="product-count">{{ $t('Common.ProductCount', { count: user.product_count }) }}</span>
+      </v-chip>
+      <v-chip v-if="user.proof_count" label size="small" density="comfortable">
+        <v-icon start icon="mdi-image" />
+        <span id="product-count">{{ $t('Common.ProofCount', { count: user.proof_count }) }}</span>
+      </v-chip>
       <UserActionMenuButton :user="user" />
     </v-card-text>
   </v-card>
