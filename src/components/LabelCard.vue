e7b53f68e46e127cef70735b1634c94f6f3631b7
Raphaël Odini <raphodn@users.noreply.github.com>
2024-09-26 23:43:40 +0200

    feat(label card): new label card with action menu (#849)

---
+++ src/components/LabelCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card :title="label" prepend-icon="mdi-sticker-text-outline" data-name="label-card">
+    <v-card-text>
+      <ProductCountChip :count="productCount" :withLabel="true" />
+      <LabelActionMenuButton :label="label" />
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+export default {
+  components: {
+    ProductCountChip: defineAsyncComponent(() => import('../components/ProductCountChip.vue')),
+    LabelActionMenuButton: defineAsyncComponent(() => import('../components/LabelActionMenuButton.vue'))
+  },
+  props: {
+    label: {
+      type: String,
+      default: null
+    },
+    productCount: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
