105d828f2d8dedb0b6fe036e5df4db55d66a4587
Alex Bourreau <github@bourreau.dev>
2024-09-27 23:19:59 +0200

    fix(Category detail): properly refresh the list when choosing another category (#844)

--- src/views/CategoryDetail.vue
+++ src/views/CategoryDetail.vue
@@ -69,6 +69,7 @@ export default {
   watch: {
     $route (newCategory, oldCategory) {
       if (oldCategory && newCategory && newCategory.name == 'category-detail' && oldCategory.fullPath != newCategory.fullPath) {
+        this.categoryId = newCategory.params.id
         this.initCategory()
       }
     }
