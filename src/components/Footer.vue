6d0536748591f1264e8872b10831d73e46589c0c
Raphael Odini <raphodn@users.noreply.github.com>
2024-09-29 11:23:42 +0200

    fix(Footer): reduce height, fix bottom padding, add inside padding. ref #843

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-footer class="bg-grey-lighten-1" style="max-height: 20%">
-    <v-row>
+  <v-footer class="bg-grey-lighten-1 py-4">
+    <v-row no-gutters>
       <v-col cols="12" md="6" align="center">
         <i18n-t keypath="Footer.TagLine" tag="span">
           <template #name>
