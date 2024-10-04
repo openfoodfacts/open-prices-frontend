1149895ff10bfd5d3add717e8f2c9752899ea80d
Raphael Odini <raphodn@users.noreply.github.com>
2024-10-04 11:21:02 +0200

    Manage translations

--- src/components/Breadcrumbs.vue
+++ src/components/Breadcrumbs.vue
@@ -1,5 +1,9 @@
 <template>
-  <v-breadcrumbs v-if="breadcrumbs" class="text-h5 px-0 pt-0" density="compact" :items="breadcrumbs" />
+  <v-breadcrumbs v-if="breadcrumbs" class="text-h5 px-0 pt-0" density="compact" :items="breadcrumbs">
+    <template #title="{ item }">
+      {{ $t(`Router.${item.title}.Title`) }}
+    </template>
+  </v-breadcrumbs>
 </template>
   
 <script>
