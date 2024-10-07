8816898baafff10faf233d5c27742072d3daafee
Raphael Odini <raphodn@users.noreply.github.com>
2024-10-08 00:59:43 +0200

    refactor(Sign in): on Sign in success, redirect to dashboard. ref #878

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -75,7 +75,7 @@ export default {
         .then((data) => {
           if (data['access_token']) {
             this.appStore.signIn(this.signinForm.username.toLowerCase().trim(), data['access_token'])
-            this.$router.push({ path: '/prices', query: { signinSuccess: 'true' } })
+            this.done()
           } else {
             alert(this.$t('SignIn.WrongCredentials'))
             this.loading = false
@@ -86,6 +86,9 @@ export default {
           this.loading = false
         });
     },
+    done() {
+      this.$router.push({ path: '/dashboard', query: { signinSuccess: 'true' } })
+    }
   },
 };
 </script>
