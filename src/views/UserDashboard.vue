3e7e57dfbb8a9e51fa9893cad052d306c17ed94a
Raphael Odini <raphodn@users.noreply.github.com>
2024-10-08 00:06:17 +0200

    Load & display latest prices

--- src/views/UserDashboard.vue
+++ src/views/UserDashboard.vue
@@ -6,23 +6,38 @@
   </v-row>
 
   <v-row>
-    <v-col cols="12" sm="6" lg="4">
+    <v-col cols="6" sm="4" md="3" lg="2">
       <v-card
         :title="$t('UserDashboard.MyPrices')"
-        :subtitle="userPriceTotal"
+        :subtitle="userPriceCount"
         prepend-icon="mdi-tag-multiple-outline"
         height="100%"
         to="/dashboard/prices"
       />
     </v-col>
-    <v-col cols="12" sm="6" lg="4">
+    <v-col cols="6" sm="4" md="3" lg="2">
       <v-card
         :title="$t('UserDashboard.MyProofs')"
-        :subtitle="userProofTotal"
+        :subtitle="userProofCount"
         prepend-icon="mdi-image"
         height="100%"
         to="/dashboard/proofs"
       />
+    </v-col>
+  </v-row>
+
+  <br>
+
+  <v-row>
+    <v-col v-for="price in displayedPriceList" :key="price" cols="12" sm="6" md="4" xl="3">
+      <PriceCard :price="price" :product="price.product" :hidePriceCreated="false" elevation="1" height="100%" />
+    </v-col>
+    <v-col cols="12" sm="6" md="4" xl="3" align="center">
+      <br v-if="$vuetify.display.smAndUp"><!-- TODO: center vertically instead of br -->
+      <br v-if="$vuetify.display.smAndUp">
+      <v-btn to="/dashboard/prices" prepend-icon="mdi-tag-multiple-outline" append-icon="mdi-arrow-right">
+        {{ $t('UserDashboard.MyPrices') }}
+      </v-btn>
     </v-col>
   </v-row>
 </template>
@@ -36,12 +51,14 @@ import api from '../services/api'
 export default {
   components: {
     UserCard: defineAsyncComponent(() => import('../components/UserCard.vue')),
+    PriceCard: defineAsyncComponent(() => import('../components/PriceCard.vue'))
   },
   data() {
     return {
       user: null,
-      userPriceTotal: null,
-      userProofTotal: null,
+      userPriceCount: null,
+      userProofCount: null,
+      userPriceList: [],
       loading: false,
     }
   },
@@ -50,11 +67,18 @@ export default {
     username() {
       return this.appStore.user.username
     },
+    displayedPriceList() {
+      if (!this.$vuetify.display.smAndUp) {
+        return this.userPriceList.slice(0, 5)
+      } else {
+        return this.userPriceList
+      }
+    }
   },
   mounted() {
     this.getUser()
-    this.getUserPriceCount()
     this.getUserProofCount()
+    this.getPrices()
   },
   methods: {
     getUser() {
@@ -65,19 +89,20 @@ export default {
           this.loading = false
         })
     },
-    getUserPriceCount() {
-      this.loading = true
-      return api.getPrices({ owner: this.username, size: 1 })
-        .then((data) => {
-          this.userPriceTotal = data.total
-          this.loading = false
-        })
-    },
     getUserProofCount() {
       this.loading = true
       return api.getProofs({ owner: this.username, size: 1 })
         .then((data) => {
-          this.userProofTotal = data.total
+          this.userProofCount = data.total
+          this.loading = false
+        })
+    },
+    getPrices() {
+      this.loading = true
+      return api.getPrices({ owner: this.username, size: 25 })
+        .then((data) => {
+          this.userPriceList = data.items
+          this.userPriceCount = data.total
           this.loading = false
         })
     },
