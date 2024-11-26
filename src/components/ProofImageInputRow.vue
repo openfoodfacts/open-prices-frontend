40301dc00b4a094b6ff0b9bccdb36d63bc0c1807
Raphael Odini <raphodn@users.noreply.github.com>
2024-11-26 01:23:45 +0100

    fix(Proofs): fix selecting existing proof, following #1043

--- src/components/ProofImageInputRow.vue
+++ src/components/ProofImageInputRow.vue
@@ -149,6 +149,9 @@ export default {
       this.proofImageForm.proof_id = null
     },
     getLocalProofUrl(blob) {
+      if (blob.image_thumb_path) {
+        return `${import.meta.env.VITE_OPEN_PRICES_APP_URL}/img/${blob.image_thumb_path}`
+      }
       // return 'https://prices.openfoodfacts.org/img/0002/qU59gK8PQw.webp'  // PRICE_TAG
       // return 'https://prices.openfoodfacts.net/img/0001/lZGFga9ZOT.webp'  // RECEIPT
       return URL.createObjectURL(blob)
