a4b1bf591c9e674a34b10cf4cf8757245f9e5f34
Raphael Odini <raphodn@users.noreply.github.com>
2024-12-02 23:15:40 +0100

    New ProofReceiptPriceCountChip & ProofReceiptPriceTotalChip components

---
+++ src/components/ProofReceiptPriceTotalChip.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-chip label size="small" variant="flat" density="comfortable" :title="$t('Common.ReceiptPriceTotal')">
+    {{ getPriceValueDisplay(count) }}
+  </v-chip>
+</template>
+
+<script>
+import utils from '../utils.js'
+
+export default {
+  props: {
+    count: {
+      type: Number,
+      default: null
+    },
+    currency: {
+      type: String,
+      default: null
+    }
+  },
+  methods: {
+    getPriceValue(priceValue, priceCurrency) {
+      return utils.prettyPrice(priceValue, priceCurrency)
+    },
+    getPriceValueDisplay(price) {
+      price = parseFloat(price)
+      return this.getPriceValue(price, this.currency)
+    },
+  }
+}
+</script>
