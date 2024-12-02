ad3618aa4e857b29ad7185d631b8e38d04e26fd2
Raphaël Odini <raphodn@users.noreply.github.com>
2024-12-02 23:20:03 +0100

    refactor(Proof card): new components for receipt price_count & price_total (#1075)

---
+++ src/components/ProofReceiptPriceCountChip.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-chip class="mr-1" label size="small" variant="flat" density="comfortable" :title="$t('Common.ReceiptPriceCount')">
+    {{ $t('Common.PriceCount', { count: count }) }}
+  </v-chip>
+</template>
+
+<script>
+export default {
+  props: {
+    count: {
+      type: Number,
+      default: null
+    },
+  }
+}
+</script>
