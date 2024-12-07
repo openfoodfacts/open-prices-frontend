da0c4b28f5313232980b7f1d18c3f1ea03299ba2
Raphael Odini <raphodn@users.noreply.github.com>
2024-12-08 00:03:04 +0100

    refactor(Price add): small refactoring and fixes following recent Price add changes

--- src/components/ProofTypeInputRow.vue
+++ src/components/ProofTypeInputRow.vue
@@ -1,8 +1,8 @@
 <template>
   <v-row>
     <v-col cols="12">
-      <v-btn-group divided density="compact" elevation="1" :style="proofTypeForm.type ? '' : 'border: 1px solid #F44336'">
-        <v-btn v-for="pt in proofTypeList" :key="pt.key" size="small" :value="pt.key" :prepend-icon="pt.icon" :style="isSelectedType(pt.key) ? 'border: 1px solid #4CAF50' : ''" @click="proofTypeForm.type = pt.key">
+      <v-btn-group divided density="compact" elevation="1" :class="proofTypeForm.type ? '' : 'border-error'">
+        <v-btn v-for="pt in proofTypeList" :key="pt.key" size="small" :value="pt.key" :prepend-icon="pt.icon" :class="isSelectedType(pt.key) ? 'border-success' : ''" @click="proofTypeForm.type = pt.key">
           {{ $t('ProofCard.' + pt.value) }}
         </v-btn>
       </v-btn-group>
