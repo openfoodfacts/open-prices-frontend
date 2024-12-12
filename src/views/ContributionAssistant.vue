05a94df5ff67368bdbf13147397941821cd3cce6
TTalex <github@bourreau.dev>
2024-12-13 00:19:10 +0100

    feat(ContributionAssistant): Add support for automatic label detection

--- src/views/ContributionAssistant.vue
+++ src/views/ContributionAssistant.vue
@@ -26,13 +26,24 @@
       </v-tabs-window-item>
       <v-tabs-window-item value="Crop">
         <v-container>
+          <v-alert v-if="drawCanvasLoaded && !seedCrops.length && !predictionLoading" type="info" variant="outlined" icon="mdi-alert">
+            No labels could be automatically detected. Please manually draw squares around the labels or press the button below to try again.
+            <br />
+            <v-btn @click="loadPredictions(proofForm.id)">
+              Automatically find labels
+            </v-btn>
+          </v-alert>
+          <v-alert v-if="drawCanvasLoaded && predictionLoading" type="info" variant="outlined" icon="mdi-magnify">
+            Automatic label detection is running. Please wait...
+              <v-progress-circular indeterminate />
+          </v-alert>
           <v-row>
             <v-col cols="12" lg="6">
               <h3 class="mb-4">
                 1. Draw squares around the labels
               </h3>
               <v-progress-circular v-if="!drawCanvasLoaded" indeterminate />
-              <ContributionAssistantDrawCanvas ref="ContributionAssistantdrawCanvas" :image="image" @croppedImages="onCroppedImages($event)" @loaded="drawCanvasLoaded = true" />
+              <ContributionAssistantDrawCanvas ref="ContributionAssistantdrawCanvas" :image="image" :seedCrops="seedCrops" @croppedImages="onCroppedImages($event)" @loaded="drawCanvasLoaded = true" />
             </v-col>
             <v-col cols="12" lg="6">
               <h3 class="mb-4">
@@ -137,6 +148,7 @@ export default {
       tab: 'ProofSelect',
       drawCanvasLoaded: false,
       image: new Image(),
+      seedCrops: [],
       croppedImages: [],
       croppedBlobs: [],
       productPriceForms : [],
@@ -153,6 +165,7 @@ export default {
       },
       processCroppedImagesLoading: false,
       addPricesLoading: false,
+      predictionLoading: false,
       numberOfPricesAdded: 0
     }
   },
@@ -208,6 +221,24 @@ export default {
       this.croppedBlobs = []
       this.productPriceForms = []
       this.tab = "Crop"
+      this.predictionLoading = true
+      // Give the ml some time to run (5 seconds)
+      setTimeout(() => this.loadPredictions(event.id), 5000)
+    },
+    loadPredictions(proofId) {
+      this.predictionLoading = true
+      api.getProofById(proofId).then((proof) => {
+        if (proof.predictions && proof.predictions.length) {
+          for (let prediction of proof.predictions) {
+            if (prediction.type === "OBJECT_DETECTION") {
+              this.seedCrops = prediction.data.objects.map(predictionObject => {
+                return predictionObject.bounding_box
+              })
+            }
+          }
+        }
+        this.predictionLoading = false
+      })
     },
     onCroppedImages(eventData) {
       this.croppedImages = eventData[0]
