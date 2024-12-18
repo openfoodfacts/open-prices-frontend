2f02ddfceb04fc2ab7ffcac94dc39521ed15cf82
Raphael Odini <raphodn@users.noreply.github.com>
2024-12-18 17:05:10 +0100

    refactor(Price list): add new filter by type (Barcode or Category)

--- src/views/PriceList.vue
+++ src/views/PriceList.vue
@@ -5,6 +5,7 @@
         {{ $t('Common.PriceCount', { count: priceTotal }) }}
       </v-chip>
       <LoadedCountChip :loadedCount="priceList.length" :totalCount="priceTotal" />
+      <FilterMenu kind="price" :currentFilter="currentFilter" :currentType="currentType" @update:currentFilter="togglePriceFilter($event)" @update:currentType="togglePriceType($event)" />
     </v-col>
   </v-row>
 
@@ -24,12 +25,13 @@
 <script>
 import { defineAsyncComponent } from 'vue'
 import api from '../services/api'
+import constants from '../constants'
 import utils from '../utils.js'
-
 
 export default {
   components: {
     LoadedCountChip: defineAsyncComponent(() => import('../components/LoadedCountChip.vue')),
+    FilterMenu: defineAsyncComponent(() => import('../components/FilterMenu.vue')),
     PriceCard: defineAsyncComponent(() => import('../components/PriceCard.vue'))
   },
   data() {
@@ -38,12 +40,37 @@ export default {
       priceTotal: null,
       pricePage: 0,
       loading: false,
-      // scroll
-      handleDebouncedScroll: null,
+      // filter & order
+      currentFilter: '',
+      currentType: '',
+      currentOrder: constants.PRICE_ORDER_LIST[2].key,  // created first
+    }
+  },
+  computed: {
+    getPricesParams() {
+      let defaultParams = { order_by: this.currentOrder, page: this.pricePage }
+      if (this.currentFilter === 'show_last_month') {
+        let oneMonthAgo = new Date()
+        oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1)
+        defaultParams['date__gte'] = oneMonthAgo.toISOString().substring(0, 10)
+      }
+      if (this.currentType) {
+        defaultParams[constants.TYPE_PARAM] = this.currentType
+      }
+      return defaultParams
+    },
+  },
+  watch: {
+    $route (newRoute, oldRoute) { // only called when query changes to avoid having an API call when the path changes
+      if (oldRoute.path === newRoute.path && JSON.stringify(oldRoute.query) !== JSON.stringify(newRoute.query)) {
+        this.initPriceList()
+      }
     }
   },
   mounted() {
-    this.getPrices()
+    this.currentFilter = this.$route.query[constants.FILTER_PARAM] || this.currentFilter
+    this.currentType = this.$route.query[constants.TYPE_PARAM] || this.currentType
+    this.initPriceList()
     // load more
     this.handleDebouncedScroll = utils.debounce(this.handleScroll, 100)
     window.addEventListener('scroll', this.handleDebouncedScroll)
@@ -52,16 +79,32 @@ export default {
     window.removeEventListener('scroll', this.handleDebouncedScroll)
   },
   methods: {
+    initPriceList() {
+      this.priceList = []
+      this.priceTotal = null
+      this.pricePage = 0
+      this.getPrices()
+    },
     getPrices() {
       if (this.priceTotal && (this.priceList.length >= this.priceTotal)) return
       this.loading = true
       this.pricePage += 1
-      return api.getPrices({ page: this.pricePage })
+      return api.getPrices(this.getPricesParams)
         .then((data) => {
           this.priceList.push(...data.items)
           this.priceTotal = data.total
           this.loading = false
         })
+    },
+    togglePriceFilter(filterKey) {
+      this.currentFilter = this.currentFilter ? '' : filterKey
+      this.$router.push({ query: { ...this.$route.query, [constants.FILTER_PARAM]: this.currentFilter } })
+      // this.initPriceList() will be called in watch $route
+    },
+    togglePriceType(sourceKey) {
+      this.currentType = (this.currentType !== sourceKey) ? sourceKey : ''
+      this.$router.push({ query: { ...this.$route.query, [constants.TYPE_PARAM]: this.currentType } })
+      // this.initPriceList() will be called in watch $route
     },
     handleScroll(event) {  // eslint-disable-line no-unused-vars
       if (utils.getDocumentScrollPercentage() > 90) {
