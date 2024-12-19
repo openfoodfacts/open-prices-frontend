6740a37dabc4a8c28b3dbda2e77020172285832c
Raphael Odini <raphodn@users.noreply.github.com>
2024-12-19 18:31:50 +0100

    New ProofImageCropped component. Try to use for PriceValidatorAssistant

---
+++ src/components/ProofImageCropped.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-img
+    height="200px"
+    :src="croppedImage"
+    contain
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    proofImageFilePath: {
+      type: String,
+      default: ''
+    },
+    boundingBox: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      canvas: null,
+      ctx: null,
+      proofImage: null,
+      croppedImage: null
+    }
+  },
+  mounted() {
+    this.getImage()
+  },
+  methods: {
+    getImage() {
+      this.canvas = document.createElement('canvas')
+      this.ctx = this.canvas.getContext('2d')
+      this.proofImage = new Image()
+      this.proofImage.onload = this.cropImage
+      this.proofImage.src = `${import.meta.env.VITE_OPEN_PRICES_APP_URL}/img/${this.proofImageFilePath}`
+      this.proofImage.crossOrigin = 'Anonymous'
+    },
+    cropImage() {
+      console.log(this.proofImage.complete)
+      const { startX, startY, endX, endY } = this.boundingBox
+      const width = Math.abs(endX - startX)
+      const height = Math.abs(endY - startY)
+      this.canvas.width = width
+      this.canvas.height = height
+      this.ctx.drawImage(this.proofImage, Math.min(startX, endX), Math.min(startY, endY), width, height, 0, 0, width, height)
+      this.croppedImage = this.canvas.toDataURL()
+      console.log(this.croppedImage)
+    }
+  }
+}
+</script>
