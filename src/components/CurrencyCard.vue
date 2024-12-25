e74b4ad0bcd4467eea154c648a9153f45dbee076
Raphael Odini <raphodn@users.noreply.github.com>
2024-12-25 17:27:26 +0100

    New CurrencyCard component

---
+++ src/components/CurrencyCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card :title="currency" prepend-icon="mdi-cash" data-name="currency-card">
+    <v-card-text>
+      <PriceCountChip :count="priceCount" :withLabel="true" />
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+export default {
+  components: {
+    PriceCountChip: defineAsyncComponent(() => import('../components/PriceCountChip.vue')),
+  },
+  props: {
+    currency: {
+      type: String,
+      default: null
+    },
+    priceCount: {
+      type: Number,
+      default: 0
+    }
+  },
+}
+</script>
