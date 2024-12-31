300d4bb34a9d13296b00149df3e8c68571dc98a8
Raphaël Odini <raphodn@users.noreply.github.com>
2024-12-31 16:26:50 +0100

    refactor(Proofs): new ProofDialog component (#1210)

---
+++ src/components/ProofDialog.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-dialog scrollable max-height="80%" min-width="50%" width="auto">
+    <ProofCard :proof="proof" :hideProofActions="true" :readonly="readonly" @close="close" />
+  </v-dialog>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+export default {
+  components: {
+    ProofCard: defineAsyncComponent(() => import('../components/ProofCard.vue')),
+  },
+  props: {
+    proof: {
+      type: Object,
+      default: null
+    },
+    readonly: {
+      type: Boolean,
+      default: false,
+    }
+  },
+  emits: ['close'],
+  methods: {
+    close() {
+      this.$emit('close')
+    },
+  }
+}
+</script>
