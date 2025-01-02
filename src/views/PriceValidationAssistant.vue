33f6aafac4581adba14cde03da597805d7a29daf
Raphael Odini <raphodn@users.noreply.github.com>
2025-01-02 12:10:41 +0100

    feat(Price Validator Assistant): open price validation to all proofs (not just owned)

--- src/views/PriceValidationAssistant.vue
+++ src/views/PriceValidationAssistant.vue
@@ -72,7 +72,13 @@ export default {
       return 6
     },
     getPriceTagsParams() {
-      return { proof__owner: this.username, proof__ready_for_price_tag_validation: true, status__isnull: true, order_by: this.currentOrder, size: this.getApiSize, page: this.priceTagPage }
+      return {
+        proof__ready_for_price_tag_validation: true,
+        status__isnull: true,
+        order_by: this.currentOrder,
+        size: this.getApiSize,
+        page: this.priceTagPage
+      }
     },
   },
   mounted() {
