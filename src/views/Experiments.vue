e004d23076b8aa8f2b610e5586894e16b897d2c1
Raphael Odini <raphodn@users.noreply.github.com>
2025-01-04 00:41:52 +0100

    refactor(Experiments): move down single price & proof workflows

--- src/views/Experiments.vue
+++ src/views/Experiments.vue
@@ -2,20 +2,6 @@
   <v-row>
     <v-col cols="12" sm="6" lg="4">
       <v-card
-        :title="$t('Router.PriceAddSingle.Title')"
-        prepend-icon="mdi-tag-plus-outline"
-        to="/prices/add/single"
-      />
-    </v-col>
-    <v-col cols="12" sm="6" lg="4">
-      <v-card
-        :title="$t('Router.ProofAddSingle.Title')"
-        prepend-icon="mdi-image-plus"
-        to="/proofs/add/single"
-      />
-    </v-col>
-    <v-col cols="12" sm="6" lg="4">
-      <v-card
         :title="$t('Router.ProofAddMultiple.Title')"
         prepend-icon="mdi-image-multiple"
         to="/proofs/add/multiple"
@@ -33,6 +19,25 @@
         :title="$t('Router.PriceValidationAssistant.Title')"
         prepend-icon="mdi-checkbox-marked-circle-plus-outline"
         to="/experiments/price-validation-assistant"
+      />
+    </v-col>
+  </v-row>
+
+  <br>
+
+  <v-row>
+    <v-col cols="12" sm="6" lg="4">
+      <v-card
+        :title="$t('Router.PriceAddSingle.Title')"
+        prepend-icon="mdi-tag-plus-outline"
+        to="/prices/add/single"
+      />
+    </v-col>
+    <v-col cols="12" sm="6" lg="4">
+      <v-card
+        :title="$t('Router.ProofAddSingle.Title')"
+        prepend-icon="mdi-image-plus"
+        to="/proofs/add/single"
       />
     </v-col>
   </v-row>
