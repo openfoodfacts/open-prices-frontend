1586b02e53a13f3a40ff82be9c0333838d2c96c7
Raphael Odini <raphodn@users.noreply.github.com>
2025-01-08 15:47:10 +0100

    refactor(Breadcrumbs): reduce padding to gain some px

--- src/components/Breadcrumbs.vue
+++ src/components/Breadcrumbs.vue
@@ -1,16 +1,25 @@
 <template>
-  <v-breadcrumbs v-if="breadcrumbs" class="text-h6 pa-0" density="compact" :items="breadcrumbs">
-    <template #title="{ item }">
-      {{ $t(`Router.${item.title}.Title`) }}
-    </template>
-  </v-breadcrumbs>
+  <v-row v-if="breadcrumbs">
+    <v-col class="py-0">
+      <v-breadcrumbs class="text-h6 pa-0" density="compact" :items="breadcrumbs">
+        <template #item="{ item }">
+          <v-breadcrumbs-item
+            class="pa-0"
+            :title="$t(`Router.${item.title}.Title`)"
+            :to="item.to"
+            :disabled="item.disabled"
+          />
+        </template>
+      </v-breadcrumbs>
+    </v-col>
+  </v-row>
 </template>
   
 <script>
 export default {
   computed: {
     breadcrumbs() {
-      return this.$route.meta.breadcrumbs;
+      return this.$route.meta.breadcrumbs
     }
   },
 }
