931825f8edf194ad7a22a60dc8c9cd449ecc38b9
Raphael Odini <raphodn@users.noreply.github.com>
2025-01-26 22:55:03 +0100

    fix(User card): avoid overlap between chips and action menu

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -6,19 +6,24 @@
     @click="goToUser(user)"
   >
     <v-card-text>
-      <PriceCountChip :count="user.price_count" :withLabel="true" />
-      <v-chip v-if="user.location_count" label size="small" density="comfortable" class="mr-1">
-        <v-icon start icon="mdi-map-marker-outline" />
-        <span id="location-count">{{ $t('Common.LocationCount', { count: user.location_count }) }}</span>
-      </v-chip>
-      <v-chip v-if="user.product_count" label size="small" density="comfortable" class="mr-1">
-        <v-icon start icon="mdi-database-outline" />
-        <span id="product-count">{{ $t('Common.ProductCount', { count: user.product_count }) }}</span>
-      </v-chip>
-      <v-chip v-if="user.proof_count" label size="small" density="comfortable" :to="getUserProofListUrl">
-        <v-icon start icon="mdi-image" />
-        <span id="proof-count">{{ $t('Common.ProofCount', { count: user.proof_count }) }}</span>
-      </v-chip>
+      <v-row>
+        <v-col cols="11">
+          <PriceCountChip :count="user.price_count" :withLabel="true" />
+          <v-chip v-if="user.location_count" label size="small" density="comfortable" class="mr-1">
+            <v-icon start icon="mdi-map-marker-outline" />
+            <span id="location-count">{{ $t('Common.LocationCount', { count: user.location_count }) }}</span>
+          </v-chip>
+          <v-chip v-if="user.product_count" label size="small" density="comfortable" class="mr-1">
+            <v-icon start icon="mdi-database-outline" />
+            <span id="product-count">{{ $t('Common.ProductCount', { count: user.product_count }) }}</span>
+          </v-chip>
+          <v-chip v-if="user.proof_count" label size="small" density="comfortable" :to="getUserProofListUrl">
+            <v-icon start icon="mdi-image" />
+            <span id="proof-count">{{ $t('Common.ProofCount', { count: user.proof_count }) }}</span>
+          </v-chip>
+        </v-col>
+      </v-row>
+
       <UserActionMenuButton :user="user" />
     </v-card-text>
   </v-card>
