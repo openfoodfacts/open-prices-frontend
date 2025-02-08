2428972377ff123ee2a426b9bdbdfa6df73e7ea7
Raphael Odini <raphodn@users.noreply.github.com>
2025-02-08 13:10:08 +0100

    new ProductSourceChip

---
+++ src/components/ProductSourceChip.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-chip
+    :style="style"
+    label
+    size="small"
+    density="comfortable"
+  >
+    <v-icon :icon="getProductSourceIcon" />
+  </v-chip>
+</template>
+
+<script>
+import constants from '../constants'
+
+export default {
+  props: {
+    product: {
+      type: Object,
+      default: null
+    },
+    style: {
+      type: String,
+      default: 'padding-left:5px;padding-right:5px;'
+    },
+  },
+  computed: {
+    getProductSourceIcon() {
+      if (this.product.source) {
+        return constants[`${this.product.source.toUpperCase()}_ICON`]
+      }
+      return null
+    },
+  }
+}
+</script>
