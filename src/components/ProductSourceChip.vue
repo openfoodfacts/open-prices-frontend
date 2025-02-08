e06623c88a60c8c02b806b9bbc8e2f75d81d9f84
Raphaël Odini <raphodn@users.noreply.github.com>
2025-02-08 13:18:42 +0100

    feat(Products): new ProductSourceChip to display the product's source (OxF) (#1363)

---
+++ src/components/ProductSourceChip.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-chip
+    :style="style"
+    label
+    size="small"
+    density="comfortable"
+  >
+    <v-icon :icon="getProductSourceIcon" />
+    <v-tooltip activator="parent" open-on-click location="top">
+      {{ getProductSourceName }}
+    </v-tooltip>
+  </v-chip>
+</template>
+
+<script>
+import constants from '../constants'
+
+export default {
+  props: {
+    product: {
+      type: Object,
+      default: null
+    },
+    style: {
+      type: String,
+      default: 'padding-left:5px;padding-right:5px;'
+    },
+  },
+  computed: {
+    getProductSourceIcon() {
+      if (this.product.source) {
+        return constants[`${this.product.source.toUpperCase()}_ICON`]
+      }
+      return null
+    },
+    getProductSourceName() {
+      if (this.product.source) {
+        return constants[`${this.product.source.toUpperCase()}_NAME`]
+      }
+      return null
+    },
+  }
+}
+</script>
