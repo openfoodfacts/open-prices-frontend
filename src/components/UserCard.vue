25674cbe4ce0961ac0da2c240514a648f065fd0b
Raphael Odini <raphodn@users.noreply.github.com>
2025-02-09 23:39:18 +0100

    fix(User Card): fix missing currency count chip margin. ref #1352

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -9,7 +9,7 @@
       <v-row>
         <v-col :cols="hideActionMenuButton ? '12' : '11'">
           <PriceCountChip :count="user.price_count" :withLabel="true" />
-          <v-chip v-if="user.price_currency_count > 1" label size="small" density="comfortable">
+          <v-chip v-if="user.price_currency_count > 1" label size="small" density="comfortable" class="mr-1">
             <v-icon start icon="mdi-cash" />
             <span id="product-count">{{ $t('Common.CurrencyCount', { count: user.price_currency_count }) }}</span>
           </v-chip>
