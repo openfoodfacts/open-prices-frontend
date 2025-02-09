88e074bbe49321772990562d39185f14e1dcebd7
Raphaël Odini <raphodn@users.noreply.github.com>
2025-02-10 00:47:53 +0100

    refactor(Proofs): new ProofCountChip component (#1371)

---
+++ src/components/ProofCountChip.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-chip label size="small" density="comfortable">
+    <v-icon start :icon="PROOF_ICON" />
+    <span v-if="withLabel" id="proof-count">{{ $t('Common.ProofCount', { count: count }) }}</span>
+    <span v-else id="proof-count">{{ count }}</span>
+  </v-chip>
+</template>
+
+<script>
+import constants from '../constants'
+
+export default {
+  props: {
+    count: {
+      type: Number,
+      default: null
+    },
+    withLabel: {
+      type: Boolean,
+      default: false
+    },
+  },
+  data() {
+    return {
+      PROOF_ICON: constants.PROOF_ICON
+    }
+  }
+}
+</script>
