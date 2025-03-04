64d4f89bc409e13fc3e61f6bbbbde9acee188e76
Raphael Odini <raphodn@users.noreply.github.com>
2025-03-05 00:12:17 +0100

    New ProofReceiptOnlineDeliveryCostsChip component

---
+++ src/components/ProofReceiptOnlineDeliveryCostsChip.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-chip label size="small" :prepend-icon="LOCATION_TYPE_ONLINE_ICON" variant="flat" density="comfortable" :title="$t('Common.ReceiptOnlineDeliveryCosts')">
+    {{ getPriceValueDisplay(price) }}
+  </v-chip>
+</template>
+
+<script>
+import constants from '../constants'
+import utils from '../utils.js'
+
+export default {
+  props: {
+    price: {
+      type: Number,
+      default: null
+    },
+    currency: {
+      type: String,
+      default: null
+    }
+  },
+  data() {
+    return {
+      LOCATION_TYPE_ONLINE_ICON: constants.LOCATION_TYPE_ONLINE_ICON,
+    }
+  },
+  methods: {
+    getPriceValue(priceValue, priceCurrency) {
+      return utils.prettyPrice(priceValue, priceCurrency)
+    },
+    getPriceValueDisplay(price) {
+      price = parseFloat(price)
+      return this.getPriceValue(price, this.currency)
+    },
+  }
+}
+</script>
