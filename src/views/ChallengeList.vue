5803726d794ca202fcbcce44cc316ca9516ddeba
Alex Bourreau <7023229+TTalex@users.noreply.github.com>
2025-03-23 15:06:42 +0100

    refactor(Challenges): Remove status filter + cleanup (#1466)
    Co-authored-by: Raphael Odini <raphodn@users.noreply.github.com>

--- src/views/ChallengeList.vue
+++ src/views/ChallengeList.vue
@@ -1,16 +1,38 @@
 <template>
   <v-row v-if="!loading">
     <v-col>
-      <v-chip label variant="text" prepend-icon="mdi-tag-outline">
+      <v-chip label variant="text" prepend-icon="mdi-trophy-variant">
         {{ $t('Challenge.ChallengeCount', { count: challengeTotal }) }}
       </v-chip>
       <LoadedCountChip :loadedCount="challengeList.length" :totalCount="challengeTotal" />
-      <FilterMenu kind="challenge" :currentFilter="currentFilter" :currentType="currentType" @update:currentFilter="toggleChallengeFilter($event)" @update:currentType="toggleChallengeType($event)" />
     </v-col>
   </v-row>
 
+  <v-row>
+    <v-col>
+      <h2 class="text-h6">
+        {{ $t('Challenge.OngoingChallenges') }}
+      </h2>
+    </v-col>
+  </v-row>
   <v-row class="mt-0">
-    <v-col v-for="challenge in challengeList" :key="challenge" cols="12" sm="6" md="4" xl="3">
+    <v-col v-for="challenge in ongoingChallenges" :key="challenge" cols="12" sm="6" md="4" xl="3">
+      <ChallengeCard :challenge="challenge" />
+    </v-col>
+    <v-col v-if="ongoingChallenges.length === 0">
+      {{ $t('Challenge.NoChallengeCurrentlyOngoing') }}
+    </v-col>
+  </v-row>
+
+  <v-row>
+    <v-col>
+      <h2 class="text-h6">
+        {{ $t('Challenge.PastOrFutureChallenges') }}
+      </h2>
+    </v-col>
+  </v-row>
+  <v-row class="mt-0">
+    <v-col v-for="challenge in otherChallenges" :key="challenge" cols="12" sm="6" md="4" xl="3">
       <ChallengeCard :challenge="challenge" />
     </v-col>
   </v-row>
@@ -25,13 +47,11 @@
 <script>
 import { defineAsyncComponent } from 'vue'
 import api from '../services/api'
-import constants from '../constants'
 import utils from '../utils.js'
 
 export default {
   components: {
     LoadedCountChip: defineAsyncComponent(() => import('../components/LoadedCountChip.vue')),
-    FilterMenu: defineAsyncComponent(() => import('../components/FilterMenu.vue')),
     ChallengeCard: defineAsyncComponent(() => import('../components/ChallengeCard.vue')),
   },
   data() {
@@ -40,33 +60,22 @@ export default {
       challengeTotal: null,
       challengePage: 0,
       loading: false,
-      currentFilter: '',
-      currentType: '',
       currentOrder: 'id',
     }
   },
   computed: {
     getChallengesParams() {
-      let defaultParams = { order_by: this.currentOrder, page: this.challengePage, status: "ONGOING" }
-      if (this.currentFilter === 'show_all_status') {
-        delete defaultParams['status']
-      }
-      if (this.currentType) {
-        defaultParams[constants.TYPE_PARAM] = this.currentType
-      }
+      let defaultParams = { order_by: this.currentOrder, page: this.challengePage}
       return defaultParams
     },
-  },
-  watch: {
-    $route (newRoute, oldRoute) { // only called when query changes to avoid having an API call when the path changes
-      if (oldRoute.path === newRoute.path && JSON.stringify(oldRoute.query) !== JSON.stringify(newRoute.query)) {
-        this.initChallengeList()
-      }
-    }
+    ongoingChallenges() {
+      return this.challengeList.filter(challenge => challenge.status === "ONGOING")
+    },
+    otherChallenges() {
+      return this.challengeList.filter(challenge => challenge.status !== "ONGOING")
+    },
   },
   mounted() {
-    this.currentFilter = this.$route.query[constants.FILTER_PARAM] || this.currentFilter
-    this.currentType = this.$route.query[constants.TYPE_PARAM] || this.currentType
     this.initChallengeList()
     // load more
     this.handleDebouncedScroll = utils.debounce(this.handleScroll, 100)
@@ -92,14 +101,6 @@ export default {
           this.challengeTotal = data.total
           this.loading = false
         })
-    },
-    toggleChallengeFilter(filterKey) {
-      this.currentFilter = this.currentFilter ? '' : filterKey
-      this.$router.push({ query: { ...this.$route.query, [constants.FILTER_PARAM]: this.currentFilter } })
-    },
-    toggleChallengeType(sourceKey) {
-      this.currentType = (this.currentType !== sourceKey) ? sourceKey : ''
-      this.$router.push({ query: { ...this.$route.query, [constants.TYPE_PARAM]: this.currentType } })
     },
     handleScroll(event) {  // eslint-disable-line no-unused-vars
       if (utils.getDocumentScrollPercentage() > 90) {
