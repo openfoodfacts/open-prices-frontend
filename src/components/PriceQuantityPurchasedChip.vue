ca0fc139a44363e882ce7b740f25aa093066f21c
Raphaël Odini <raphodn@users.noreply.github.com>
2025-06-05 23:19:46 +0200

    refactor(Receipt Quantity): new PriceQuantityPurchasedChip component (#1558)

---
+++ src/components/PriceQuantityPurchasedChip.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-chip class="ml-1" variant="outlined" size="small" density="comfortable">
+    x{{ priceQuantityPurchased }}
+  </v-chip>
+</template>
+
+<script>
+export default {
+  props: {
+    priceQuantityPurchased: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
