b3714427c4da68cdadb7bd83aa05cd8855ce151d
Raphael Odini <raphodn@users.noreply.github.com>
2025-09-20 23:39:36 +0200

    refactor(User card): new CurrencyCountChip, CountryCountChip & ChallengeCountChip components

---
+++ src/components/CountryCountChip.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-chip label size="small" density="comfortable">
+    <v-icon start icon="mdi-map-outline" />
+    <span v-if="withLabel" id="country-count">{{ $t('Common.CountryCount', { count: count }) }}</span>
+    <span v-else id="country-count">{{ count }}</span>
+  </v-chip>
+</template>
+
+<script>
+export default {
+  props: {
+    count: {
+      type: Number,
+      default: null
+    },
+    withLabel: {
+      type: Boolean,
+      default: false
+    },
+  }
+}
+</script>
