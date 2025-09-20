904cd90e87b090736a089e3ca35a51ae4a508f46
Raphael Odini <raphodn@users.noreply.github.com>
2025-09-20 16:11:51 +0200

    show only 3. show locations first

--- src/components/RankingTableCard.vue
+++ src/components/RankingTableCard.vue
@@ -5,7 +5,7 @@
     </template>
   
     <v-card-text>
-      <v-data-table :headers="headers" :items="items" hide-default-header hide-default-footer items-per-page="100">
+      <v-data-table :headers="headers" :items="itemsDisplayed" hide-default-header hide-default-footer items-per-page="100">
         <template #[`item.rank`]="{ index }">
           {{ index + 1 }}
         </template>
@@ -34,6 +34,10 @@ export default {
       type: Array,
       default: () => []
     },
+    itemsDisplayedCount: {
+      type: Number,
+      default: 3
+    }
   },
   computed: {
     headers() {
@@ -44,6 +48,9 @@ export default {
         { text: this.$t('Common.Count'), key: 'count' },
       ]
       return allHeaders.filter((header, index) => index === 0 || this.items.some(item => item[header.key] !== undefined))
+    },
+    itemsDisplayed() {
+      return this.items.slice(0, this.itemsDisplayedCount)
     }
   }
 }
