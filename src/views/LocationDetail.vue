b339f4aa6e816fccf2e39c1b3c4ec863ac38020f
Raphael Odini <raphodn@users.noreply.github.com>
2025-10-05 19:05:59 +0200

    refactor(Location detail): add price display toggle (list or table)

--- src/views/LocationDetail.vue
+++ src/views/LocationDetail.vue
@@ -24,14 +24,22 @@
       <LoadedCountChip :loadedCount="priceList.length" :totalCount="priceTotal" />
       <FilterMenu kind="price" :currentFilterList="currentFilterList" @update:currentFilterList="updateFilterList($event)" />
       <OrderMenu kind="price" :currentOrder="currentOrder" @update:currentOrder="selectPriceOrder($event)" />
+      <DisplayMenu :show="['list', 'table']" :currentDisplay="currentDisplay" @update:currentDisplay="selectPriceDisplay($event)" />
     </v-col>
   </v-row>
 
-  <v-row class="mt-0">
-    <v-col v-for="price in priceList" :key="price" cols="12" sm="6" md="4" xl="3">
-      <PriceCard :price="price" :product="price.product" :hidePriceLocation="true" elevation="1" height="100%" />
-    </v-col>
-  </v-row>
+  <v-window v-model="currentDisplay" disabled>
+    <v-window-item value="list">
+      <v-row class="mt-0 mb-1">
+        <v-col v-for="price in priceList" :key="price" cols="12" sm="6" md="4" xl="3">
+          <PriceCard :price="price" :product="price.product" :hideProductLocation="true" elevation="1" height="100%" />
+        </v-col>
+      </v-row>
+    </v-window-item>
+    <v-window-item value="table">
+      <PriceTable class="mt-3 mb-3" :priceList="priceList" source="location" />
+    </v-window-item>
+  </v-window>
 
   <v-row v-if="loading">
     <v-col align="center">
@@ -42,6 +50,8 @@
 
 <script>
 import { defineAsyncComponent } from 'vue'
+import { mapStores } from 'pinia'
+import { useAppStore } from '../store'
 import api from '../services/api'
 import constants from '../constants'
 import date_utils from '../utils/date.js'
@@ -53,7 +63,9 @@ export default {
     LoadedCountChip: defineAsyncComponent(() => import('../components/LoadedCountChip.vue')),
     FilterMenu: defineAsyncComponent(() => import('../components/FilterMenu.vue')),
     OrderMenu: defineAsyncComponent(() => import('../components/OrderMenu.vue')),
+    DisplayMenu: defineAsyncComponent(() => import('../components/DisplayMenu.vue')),
     PriceCard: defineAsyncComponent(() => import('../components/PriceCard.vue')),
+    PriceTable: defineAsyncComponent(() => import('../components/PriceTable.vue')),
   },
   data() {
     return {
@@ -64,12 +76,14 @@ export default {
       priceTotal: null,
       pricePage: 0,
       loading: false,
-      // filter & order
+      // filter, order & display
       currentFilterList: [],
       currentOrder: constants.PRICE_ORDER_LIST[2].key,  // date
+      currentDisplay: constants.DISPLAY_LIST[0].key,
     }
   },
   computed: {
+    ...mapStores(useAppStore),
     locationFound() {
       return this.location && (this.location.osm_id || this.location.website_url)
     },
@@ -91,6 +105,7 @@ export default {
   mounted() {
     this.currentFilterList = utils.toArray(this.$route.query[constants.FILTER_PARAM]) || this.currentFilterList
     this.currentOrder = this.$route.query[constants.ORDER_PARAM] || this.currentOrder
+    this.currentDisplay = this.$route.query[constants.DISPLAY_PARAM] || this.appStore.user.price_list_display_default_mode || this.currentDisplay
     this.getLocation()
     this.getPrices()
     // load more
@@ -138,6 +153,11 @@ export default {
         this.$router.push({ query: { ...this.$route.query, [constants.ORDER_PARAM]: this.currentOrder } })
         // this.initPrices() will be called in watch $route
       }
+    },
+    selectPriceDisplay(displayKey) {
+      this.currentDisplay = displayKey
+      this.$router.push({ query: { ...this.$route.query, [constants.DISPLAY_PARAM]: this.currentDisplay } })
+      // this.initPrices() will NOT be called in watch $route
     },
     handleScroll(event) {  // eslint-disable-line no-unused-vars
       if (utils.getDocumentScrollPercentage() > 90) {
