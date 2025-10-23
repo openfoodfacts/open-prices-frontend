2cbc13401572327bbf03d2954991060b28ef5a5c
Raphael Odini <raphodn@users.noreply.github.com>
2025-10-23 18:28:32 +0200

    refactor(Login): store newly returned user is_moderator info

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -92,7 +92,7 @@ export default {
         .signIn(this.signinForm.username.toLowerCase().trim(), this.signinForm.password)
         .then((data) => {
           if (data['access_token']) {
-            this.appStore.signIn(data['access_token'])
+            this.appStore.signIn(data)
             this.done()
           } else {
             alert(this.$t('SignIn.WrongCredentials'))
