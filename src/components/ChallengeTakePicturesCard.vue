6ec8eb08cefbe50835ac2dc445c2801f4e7be2fc
Raphael Odini <raphodn@users.noreply.github.com>
2025-10-23 16:06:52 +0200

    refactor(Proof upload): rename multiple price tag upload (url, breadcrumbs)

--- src/components/ChallengeTakePicturesCard.vue
+++ src/components/ChallengeTakePicturesCard.vue
@@ -29,7 +29,7 @@
         variant="flat"
         :block="!$vuetify.display.smAndUp"
         prepend-icon="mdi-image-plus"
-        to="/proofs/add/multiple"
+        to="/proofs/add/multiple-price-tags"
       >
         {{ $t('Challenge.StepTakePictures.AddPictures') }}
       </v-btn>
