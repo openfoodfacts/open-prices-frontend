83a5bc7746ba1d6adc057a9856925ff165dd8020
Raphaël Odini <raphodn@users.noreply.github.com>
2025-10-30 17:20:20 +0100

    feat(Price edit): allow moderators to edit & delete prices they don't own (#1784)

--- src/components/PriceDeleteConfirmationDialog.vue
+++ src/components/PriceDeleteConfirmationDialog.vue
@@ -8,14 +8,33 @@
       <v-divider />
 
       <v-card-text>
+        <v-row>
+          <v-col cols="12" md="6">
+            <PriceCard v-if="price" :price="price" :product="price.product" :hidePriceFooterRow="false" :hideActionMenuButton="true" :readonly="true" />
+          </v-col>
+        </v-row>
+        <v-row v-if="!userIsPriceOwner && userIsModerator" class="mt-0">
+          <v-col cols="12">
+            <v-alert data-name="user-not-price-owner-alert" type="info" icon="mdi-account-off" variant="outlined">
+              {{ $t('Common.UserIsNotPriceOwner') }}
+            </v-alert>
+          </v-col>
+        </v-row>
+        <v-row v-if="!userIsPriceOwner && userIsModerator" class="mt-0">
+          <v-col cols="12">
+            <v-alert data-name="user-price-delete-moderator-alert" type="info" icon="mdi-shield-account" variant="outlined">
+              {{ $t('Common.UserIsModeratorCanDeletePrice') }}
+            </v-alert>
+          </v-col>
+        </v-row>
+      </v-card-text>
+
+      <v-divider />
+
+      <v-card-text>
         <p class="mb-1">
           {{ $t('PriceDelete.Confirmation') }}
         </p>
-        <v-row>
-          <v-col cols="12" md="6">
-            <PriceCard v-if="price" :price="price" :product="price.product" :hidePriceFooterRow="true" :readonly="true" />
-          </v-col>
-        </v-row>
       </v-card-text>
 
       <v-divider />
@@ -39,6 +58,8 @@
 
 <script>
 import { defineAsyncComponent } from 'vue'
+import { mapStores } from 'pinia'
+import { useAppStore } from '../store'
 import api from '../services/api'
 
 export default {
@@ -58,6 +79,16 @@ export default {
     }
   },
   computed: {
+    ...mapStores(useAppStore),
+    username() {
+      return this.appStore.user.username
+    },
+    userIsPriceOwner() {
+      return this.username && this.price && this.price.owner === this.username
+    },
+    userIsModerator() {
+      return this.username && this.appStore.user.is_moderator
+    },
     dialogHeight() {
       return this.$vuetify.display.smAndUp ? '80%' : '100%'
     },
