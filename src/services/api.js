d3f633d03298c8c3227965ccd497d251aa14f54f
Raphael Odini <raphodn@users.noreply.github.com>
2025-11-17 12:38:10 +0100

    refactor(Location selector): when searching my OSM id, remove the osm type filtering

--- src/services/api.js
+++ src/services/api.js
@@ -539,6 +539,10 @@ export default {
    * OPENSTREETMAP API
   */ 
 
+  /**
+   * Nominatim search by query
+   * @param q: search query
+   */
   openstreetmapNominatimSearch(q) {
     const url = `${constants.OSM_NOMINATIM_SEARCH_URL}?q=${q}&addressdetails=1&format=json&limit=${LOCATION_SEARCH_LIMIT}`
     return fetch(url, {
@@ -548,6 +552,10 @@ export default {
     .then((response) => response.json())
     .then((data) => data.filter(l => !constants.NOMINATIM_RESULT_TYPE_EXCLUDE_LIST.includes(l.type)))
   },
+ /**
+   * Nominatim lookup by OSM ID
+   * @param id: OSM ID (without prefix)
+   */
   openstreetmapNominatimLookup(id) {
     const url = `${constants.OSM_NOMINATIM_LOOKUP_URL}?osm_ids=N${id},W${id},R${id}&addressdetails=1&format=json`
     return fetch(url, {
@@ -555,10 +563,13 @@ export default {
       headers: OP_DEFAULT_HEADERS
     })
     .then((response) => response.json())
-    .then((data) => data.filter(l => !constants.NOMINATIM_RESULT_TYPE_EXCLUDE_LIST.includes(l.type)))
   },
-  // Photon: restrict the search to shop & amenity
-  openstreetmapPhotonSearch(q, restrictToShop=true) {
+  /**
+   * Photon search by query
+   * @param restrictToShop: restrict the search to shop & amenity
+   * @param filterResultsOnProperties: filter out results based on their properties.osm_value
+   */
+  openstreetmapPhotonSearch(q, restrictToShop=true, filterResultsOnProperties=true) {
     let url = `${constants.OSM_PHOTON_SEARCH_URL}?q=${q}&limit=${LOCATION_SEARCH_LIMIT}`
     if (restrictToShop) {
       url += '&osm_tag=shop&osm_tag=amenity'
@@ -569,8 +580,12 @@ export default {
     })
     .then((response) => response.json())
     .then(data => data.features)
-    .then((data) => data.filter(l => !constants.NOMINATIM_RESULT_TYPE_EXCLUDE_LIST.includes(l.properties.osm_value)))
+    .then((data) => data.filter(l => filterResultsOnProperties ? !constants.NOMINATIM_RESULT_TYPE_EXCLUDE_LIST.includes(l.properties.osm_value) : true))
   },
+  /**
+   * OpenStreetMap search by query
+   * @param source: 'nominatim' (default) or 'photon'
+   */
   openstreetmapSearch(q, source='nominatim') {
     if (source === 'photon') {
       return this.openstreetmapPhotonSearch(q)
