8f9dadce886fda137da0bc4500aa09451600972a
Raphael Odini <raphodn@users.noreply.github.com>
2025-12-01 00:22:43 +0100

    refactor(User Card): new UserFooterRow component

---
+++ src/components/UserFooterRow.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-row>
+    <v-col :cols="hideActionMenuButton ? '12' : '11'">
+      <PriceCountChip class="mr-1" :count="user.price_count" :withLabel="true" />
+      <CurrencyCountChip class="mr-1" :count="user.currency_count" :withLabel="true" />
+      <LocationCountChip class="mr-1" :count="user.location_count" :withLabel="true" />
+      <CountryCountChip class="mr-1" :count="user.location_type_osm_country_count" :withLabel="true" />
+      <ProductCountChip class="mr-1" :count="user.product_count" :withLabel="true" />
+      <ProofCountChip class="mr-1" :count="user.proof_count" :withLabel="true" :to="getUserProofListUrl" />
+      <ChallengeCountChip class="mr-1" :count="user.challenge_count" :withLabel="true" />
+    </v-col>
+    <v-col v-if="!hideActionMenuButton" cols="1">
+      <UserActionMenuButton :user="user" />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+export default {
+  components: {
+    PriceCountChip: defineAsyncComponent(() => import('../components/PriceCountChip.vue')),
+    CurrencyCountChip: defineAsyncComponent(() => import('../components/CurrencyCountChip.vue')),
+    LocationCountChip: defineAsyncComponent(() => import('../components/LocationCountChip.vue')),
+    CountryCountChip: defineAsyncComponent(() => import('../components/CountryCountChip.vue')),
+    ProductCountChip: defineAsyncComponent(() => import('../components/ProductCountChip.vue')),
+    ProofCountChip: defineAsyncComponent(() => import('../components/ProofCountChip.vue')),
+    ChallengeCountChip: defineAsyncComponent(() => import('../components/ChallengeCountChip.vue')),
+    UserActionMenuButton: defineAsyncComponent(() => import('../components/UserActionMenuButton.vue'))
+  },
+  props: {
+    user: {
+      type: Object,
+      default: null
+    },
+    hideActionMenuButton: {
+      type: Boolean,
+      default: false,
+    },
+    readonly: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    getUserProofListUrl() {
+      return `/users/${this.user.user_id}/proofs`
+    },
+  },
+}
+</script>
