433ee1dca515b8659c3cbd6751581b72d8b47f2e
Raphaël Odini <raphodn@users.noreply.github.com>
2025-12-19 22:43:58 +0100

    extra change in PriceCountChip

--- src/components/PriceCountChip.vue
+++ src/components/PriceCountChip.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-chip label size="small" density="comfortable" :color="getColor()" class="mr-1">
+  <v-chip label size="small" density="comfortable" :color="getColor()">
     <v-icon start icon="mdi-tag-outline" />
     <span v-if="withLabel" id="price-count">{{ $t('Common.PriceCount', { count: count }) }}</span>
     <span v-else id="price-count">{{ count }}</span>
