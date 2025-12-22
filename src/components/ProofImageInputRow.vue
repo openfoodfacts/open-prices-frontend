f4700ab5e4f1c55468b94c215bbad11cc4ea3096
Raphaël Odini <raphodn@users.noreply.github.com>
2025-12-22 22:24:06 +0100

    refactor(Proof upload): add form label for location & picture(s) input (#1919)

--- src/components/ProofImageInputRow.vue
+++ src/components/ProofImageInputRow.vue
@@ -4,7 +4,12 @@
       <v-row>
         <v-col cols="12">
           <!-- RECEIPT: warning message -->
-          <ProofReceiptWarningAlert v-if="proofIsTypeReceipt" class="mb-4" />
+          <ProofReceiptWarningAlert v-if="proofIsTypeReceipt" class="mb-3" />
+
+          <div class="text-body-2 required">
+            <span v-if="multiple">{{ $t('Common.Pictures') }}</span>
+            <span v-else>{{ $t('Common.Picture') }}</span>
+          </div>
 
           <!-- Selection menu -->
           <v-menu scroll-strategy="close" :disabled="loading">
