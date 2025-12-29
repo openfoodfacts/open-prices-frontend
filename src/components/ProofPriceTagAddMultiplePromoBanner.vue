35af34c6a7facd0224663c0cf7ebb395f4816aad
Raphael Odini <raphodn@users.noreply.github.com>
2025-12-29 16:49:24 +0100

    fix(Banners): fix banner action button position (following #1929)

--- src/components/ProofPriceTagAddMultiplePromoBanner.vue
+++ src/components/ProofPriceTagAddMultiplePromoBanner.vue
@@ -9,6 +9,7 @@
     <v-banner-text style="padding-inline-end:10px;">
       {{ $t('ProofAdd.PromoProofPriceTagAddMultiple') }}
     </v-banner-text>
+    <v-spacer /><!-- needed to push v-banner-actions to the right on big screens -->
     <v-banner-actions>
       <v-btn icon="mdi-arrow-right" :aria-label="$t('Router.AddProofsPriceTags.Title')" to="/proofs/add/multiple-price-tags" />
     </v-banner-actions>
--- src/components/ReceiptAssistantPromoBanner.vue
+++ src/components/ReceiptAssistantPromoBanner.vue
@@ -9,6 +9,7 @@
     <v-banner-text style="padding-inline-end:10px;">
       {{ $t('ProofAdd.PromoReceiptAssistant') }}
     </v-banner-text>
+    <v-spacer /><!-- needed to push v-banner-actions to the right on big screens -->
     <v-banner-actions>
       <v-btn icon="mdi-arrow-right" :aria-label="$t('Common.TryItOut')" to="/experiments/receipt-assistant" />
     </v-banner-actions>
