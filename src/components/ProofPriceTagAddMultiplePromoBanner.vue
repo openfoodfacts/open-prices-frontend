27b1a20c8968302bd83610f3f7cbeeff9ddcae13
Raphaël Odini <raphodn@users.noreply.github.com>
2026-01-04 17:30:02 +0100

    refactor(Product Detail): if product missing, new promo banner to talk about the CreateOffProduct experiment (#1959)

--- src/components/ProofPriceTagAddMultiplePromoBanner.vue
+++ src/components/ProofPriceTagAddMultiplePromoBanner.vue
@@ -4,14 +4,24 @@
     bg-color="primary"
     rounded
     density="compact"
-    @click="$router.push('/proofs/add/price-tags')"
+    @click="$router.push(url)"
   >
-    <v-banner-text style="padding-inline-end:10px;">
+    <v-banner-text>
       {{ $t('ProofAdd.PromoProofPriceTagAddMultiple') }}
     </v-banner-text>
     <v-spacer /><!-- needed to push v-banner-actions to the right on big screens -->
     <v-banner-actions>
-      <v-btn icon="mdi-arrow-right" :aria-label="$t('Router.AddProofsPriceTags.Title')" to="/proofs/add/multiple-price-tags" />
+      <v-btn icon="mdi-arrow-right" :aria-label="$t('Router.AddProofsPriceTags.Title')" :to="url" />
     </v-banner-actions>
   </v-banner>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      url: '/proofs/add/price-tags',
+    }
+  },
+}
+</script>
