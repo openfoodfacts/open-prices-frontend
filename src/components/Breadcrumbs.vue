b9ccb090ca540a5c18f62f18d30ec7a0b8c2ea96
Raphael Odini <raphodn@users.noreply.github.com>
2026-02-13 12:17:09 +0100

    feat: new HelpDialog components to display help info

--- src/components/Breadcrumbs.vue
+++ src/components/Breadcrumbs.vue
@@ -1,27 +1,56 @@
 <template>
   <v-breadcrumbs v-if="breadcrumbs" class="text-h6 px-0 pt-0 pb-4" density="compact" :items="breadcrumbs">
-    <template #item="{ item }">
-      <v-breadcrumbs-item
-        class="pa-0"
-        :title="getItemTitle(item)"
-        :to="getItemTo(item)"
-        :disabled="item.disabled"
-      />
+    <template #item="{ item, index }">
+      <div class="d-flex align-center ga-2">
+        <v-breadcrumbs-item
+          class="pa-0"
+          :title="getItemTitle(item)"
+          :to="getItemTo(item)"
+          :disabled="item.disabled"
+        />
+        <v-icon
+          v-if="showHelpIcon(item, index)"
+          icon="mdi-information-outline"
+          size="x-small"
+          class="text-primary cursor-pointer"
+          @click="showHelpDialog = true"
+        />
+      </div>
     </template>
+
+    <HelpDialog
+      v-if="showHelpDialog"
+      v-model="showHelpDialog"
+      :title="$t('Router.' + currentPageTitle + '.Title')"
+      :text="helpText"
+      @close="showHelpDialog = false"
+    />
   </v-breadcrumbs>
 </template>
 
 <script>
+import { defineAsyncComponent } from 'vue'
+
 export default {
+  components: {
+    HelpDialog: defineAsyncComponent(() => import('../components/HelpDialog.vue'))
+  },
   data() {
     return {
-      possibleParams: [':id', ':username', ':country', ':city']
+      possibleParams: [':id', ':username', ':country', ':city'],
+      showHelpDialog: false
     }
   },
   computed: {
     breadcrumbs() {
       return this.$route.meta.breadcrumbs
     },
+    currentPageTitle() {
+      return this.breadcrumbs?.[this.breadcrumbs.length - 1]?.title || ''
+    },
+    helpText() {
+      return this.$t(`Router.${this.currentPageTitle}.Help`, '')
+    }
   },
   methods: {
     getItemTitle(item) {
@@ -40,6 +69,12 @@ export default {
         }
       }
       return item.to
+    },
+    showHelpIcon(item, index) {
+      return this.isLastItem(index) && this.$t(`Router.${item.title}.Help`, '') !== ''
+    },
+    isLastItem(index) {
+      return index === this.breadcrumbs.length - 1
     }
   }
 }
---
+++ src/components/HelpDialog.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-dialog max-width="500">
+    <v-card :title="title">
+      <template #prepend>
+        <v-icon icon="mdi-information-outline" />
+      </template>
+      <template #append>
+        <v-icon icon="mdi-close" @click="close" />
+      </template>
+
+      <v-divider />
+
+      <v-card-text>
+        {{ text }}
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    }
+  },
+  emits: ['close'],
+  methods: {
+    close() {
+      this.$emit('close')
+    },
+  }
+}
+</script>
