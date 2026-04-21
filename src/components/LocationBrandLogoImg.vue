571983561d142e0085d0eef2ae14055011da9a0b
Raphaël Odini <raphodn@users.noreply.github.com>
2026-04-21 22:23:02 +0200

    refactor(Locations): improve fallback to png logo (or default) (#2168)

--- src/components/LocationBrandLogoImg.vue
+++ src/components/LocationBrandLogoImg.vue
@@ -1,6 +1,6 @@
 <template>
   <v-img
-    v-if="logo"
+    v-if="logoDisplayed"
     :src="currentSrc"
     :width="width"
     :height="height"
@@ -19,7 +19,6 @@
 import constants from '../constants'
 
 export default {
-  name: 'LocationBrandLogoImg',
   props: {
     logo: {
       type: String,
@@ -36,23 +35,26 @@ export default {
   },
   data() {
     return {
-      triedPng: false,
+      tryPngInsteadOfSvg: false,
+      fallbackToDefault: false,
       locationImageDefault: constants.LOCATION_IMAGE_DEFAULT_URL,
     }
   },
   computed: {
+    logoDisplayed() {
+      return this.logo && !this.fallbackToDefault
+    },
     currentSrc() {
       if (!this.logo) return null
-      return this.triedPng ? `${this.logo}.png` : `${this.logo}.svg`
+      return this.tryPngInsteadOfSvg ? `${this.logo}.png` : `${this.logo}.svg`
     }
   },
   methods: {
-    onError(e) {
-      if (!this.triedPng && this.logo) {
-        this.triedPng = true
-        e.target.src = `${this.logo}.png`
+    onError() {
+      if (!this.tryPngInsteadOfSvg && this.logo) {
+        this.tryPngInsteadOfSvg = true
       } else {
-        e.target.src = this.locationImageDefault
+        this.fallbackToDefault = true
       }
     }
   }
