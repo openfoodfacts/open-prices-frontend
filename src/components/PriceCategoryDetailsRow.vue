1c295d921b596bca36e03471ba1cdefad2ea4ded
Raphaël Odini <raphodn@users.noreply.github.com>
2026-05-16 18:32:24 +0200

    refactor(Origin Chip): new OriginTagChip, similar to LabelTagChip & CategoryTagChip (#2200)

--- src/components/PriceCategoryDetailsRow.vue
+++ src/components/PriceCategoryDetailsRow.vue
@@ -3,7 +3,7 @@
     <v-col cols="12" class="pt-2 pb-2">
       <span class="chip-group">
         <CategoryTagChip v-if="!hideCategoryChip" :category="price.category_tag" :localize="true" />
-        <PriceOrigins v-if="hasPriceOrigin" :priceOrigins="price.origins_tags" />
+        <OriginTagChip v-for="origin in price.origins_tags" :key="origin" :origin="origin" :localize="true" />
         <LabelTagChip v-for="label in price.labels_tags" :key="label" :label="label" :localize="true" />
       </span>
     </v-col>
@@ -16,7 +16,7 @@ import { defineAsyncComponent } from 'vue'
 export default {
   components: {
     CategoryTagChip: defineAsyncComponent(() => import('../components/CategoryTagChip.vue')),
-    PriceOrigins: defineAsyncComponent(() => import('../components/PriceOrigins.vue')),
+    OriginTagChip: defineAsyncComponent(() => import('../components/OriginTagChip.vue')),
     LabelTagChip: defineAsyncComponent(() => import('../components/LabelTagChip.vue')),
   },
   props: {
@@ -28,14 +28,6 @@ export default {
       type: Boolean,
       default: true
     }
-  },
-  computed: {
-    hasPriceOrigin() {
-      return this.price && !!this.price.origins_tags && this.price.origins_tags.length
-    },
-    hasPriceLabels() {
-      return this.price && !!this.price.labels_tags && this.price.labels_tags.length
-    },
   }
 }
 </script>
