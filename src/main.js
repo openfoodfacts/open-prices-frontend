4d580287ca676868756fbba8ed6dce180950d118
Raphael Odini <raphodn@users.noreply.github.com>
2026-05-29 18:13:17 +0200

    update config for staging & prod

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import router from './router.js'
 import { vuetify } from './plugins/vuetify.js'
 import i18n from './i18n'
 import localeManager from './i18n/localeManager.js'
-import * as Sentry from "@sentry/vue";
+import * as Sentry from "@sentry/vue"
 
 const app = createApp(App)
 
@@ -19,23 +19,30 @@ app.use(pinia)
 app.use(router)
 app.use(vuetify)
 app.use(i18n)
+
 app.use(VueMatomo, {
   host: 'https://analytics.openfoodfacts.org',
   siteId: 13,
 })
+
 const locale = localeManager.guessDefaultLocale()
 localeManager.changeLanguage(locale)
 
-// Sentry Initialisation
-if(import.meta.env.DEV){
-  Sentry.init({
-    app,
-    dsn: import.meta.env.VITE_SENTRY_DSN,
-    sendDefualtPii: false,
-  })
+// Sentry
+if (import.meta.env.VITE_SENTRY_DSN) {
+  if (["staging", "prod"].includes(import.meta.env.VITE_OPEN_PRICES_ENV)) {
+    Sentry.init({
+      app,
+      dsn: import.meta.env.VITE_SENTRY_DSN,
+      environment: import.meta.env.VITE_OPEN_PRICES_ENV,
+      sendDefualtPii: false,  // default
+      tracesSampleRate: 1  // default
+    })
+  }
 }
 
 app.mount('#app')
+
 // Matomo
 window._paq.push(['trackPageView'])
 window._paq.push(['enableLinkTracking'])
