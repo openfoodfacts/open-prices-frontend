d9cd47a2aaa45f38af0b04ed5f389da782e1251e
Raphael Odini <raphodn@users.noreply.github.com>
2026-07-07 22:55:33 +0200

    refactor(User Card): new badge_count chip

---
+++ src/components/BadgeCountChip.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-chip label size="small" density="comfortable">
+    <v-icon start :icon="badgeIcon" />
+    <span v-if="withLabel" id="badge-count">{{ $t('Common.BadgeCount', { count: count }) }}</span>
+    <span v-else id="badge-count">{{ count }}</span>
+  </v-chip>
+</template>
+
+<script>
+import constants from '../constants'
+
+export default {
+  props: {
+    count: {
+      type: Number,
+      default: null
+    },
+    withLabel: {
+      type: Boolean,
+      default: false
+    },
+  },
+  data() {
+    return {
+      badgeIcon: constants.BADGE_ICON,
+    }
+  }
+}
+</script>
