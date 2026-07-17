7f5a45db93314af8deff4e7bffcd51ebb260b0de
Raphael Odini <raphodn@users.noreply.github.com>
2026-07-17 18:34:42 +0200

    new generic CountTextChip component

---
+++ src/components/CountTextChip.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-chip label variant="text" :prepend-icon="getKindIcon">
+    {{ $t(`Common.${getKindTitlecase}Count`, { count: count }) }}
+  </v-chip>
+</template>
+
+<script>
+import constants from '../constants'
+import utils from '../utils'
+
+export default {
+  props: {
+    kind: {
+      type: String,
+      default: null,
+      examples: ['price', 'product', 'proof', 'location', 'user', 'currency', 'country', 'challenge', 'badge']
+    },
+    count: {
+      type: Number,
+      default: null
+    }
+  },
+  computed: {
+    getKindTitlecase() {
+      return utils.toTitleCase(this.kind)
+    },
+    getKindIcon() {
+      return constants[`${this.kind.toUpperCase()}_ICON`]
+    }
+  }
+}
+</script>
