0c11fd2747a4928d171fedf8628d77177ce6b348
Raphaël Odini <raphodn@users.noreply.github.com>
2026-07-31 15:03:15 +0200

    feat(History Card): show full price/product history if available (#2304)

--- src/components/HistoryCard.vue
+++ src/components/HistoryCard.vue
@@ -2,6 +2,24 @@
   <v-card :title="getTitle" data-name="history-card">
     <v-card-text class="ml-4">
       <ul>
+        <!-- history entries -->
+        <li v-for="history in historyList" :key="history.history_id">
+          <span>{{ getDateTimeFormatted(history.history_date) }} ({{ getRelativeDateTimeFormatted(history.history_date) }})</span>
+          <span> - </span>
+          <a :href="getUserDetailUrl">{{ history.history_user_id }}</a>
+          <span v-if="history.history_change_reason">
+            <span> - </span>
+            <span>{{ history.history_change_reason }}</span>
+          </span>
+          <ul v-if="history.changes" class="ml-4">
+            <li v-for="change in history.changes" :key="change.field">
+              <span>{{ change.field }}: </span>
+              <span class="text-decoration-line-through">{{ change.old }}</span>
+              <span> → </span>
+              <span>{{ change.new }}</span>
+            </li>
+          </ul>
+        </li>
         <!-- object creation -->
         <li>
           <span>{{ getDateTimeFormatted(object.created) }} ({{ getRelativeDateTimeFormatted(object.created) }})</span>
@@ -19,6 +37,7 @@
 </template>
 
 <script>
+import openPricesApi from '../services/openPricesApi'
 import date_utils from '../utils/date.js'
 
 export default {
@@ -31,6 +50,11 @@ export default {
       type: String,
       default: null,
       examples: ['price', 'proof']
+    }
+  },
+  data() {
+    return {
+      historyList: []
     }
   },
   computed: {
@@ -47,7 +71,22 @@ export default {
       return `/users/${this.object.owner}`
     }
   },
+  mounted() {
+    this.getObjectHistory()
+  },
   methods: {
+    getObjectHistory() {
+      // only fetch history if the object has actually been updated
+      if (this.object.created == this.object.updated) return
+      openPricesApi.getHistory(this.kind, this.object.id)
+        .then(data => {
+          // filter out the object creation entry
+          this.historyList = data.filter(entry => entry.history_type !== '+')
+        })
+        .catch(error => {
+          console.error('Error fetching full history:', error)
+        })
+    },
     getDateTimeFormatted(dateTimeString) {
       return date_utils.offDateTime(dateTimeString)
     },
