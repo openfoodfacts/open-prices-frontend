229dc37d887cc86ab5d4f936a31387208f81c27f
Raphael Odini <raphodn@users.noreply.github.com>
2026-09-19 00:18:33 +0200

    bigger image

--- src/components/ContributeCard.vue
+++ src/components/ContributeCard.vue
@@ -1,8 +1,10 @@
 <template>
   <v-card :to="contribute.url">
     <div class="d-flex justify-space-between">
-      <v-card-text class="text-h6">{{ contribute.name }}</v-card-text>
-      <v-avatar class="ma-3" rounded="0" size="125">
+      <v-card-text class="text-h6">
+        {{ contribute.name }}
+      </v-card-text>
+      <v-avatar class="ma-3" rounded="0" size="150">
         <v-img v-if="contribute.background_image_url" :src="contribute.background_image_url" height="200px" cover />
       </v-avatar>
     </div>
